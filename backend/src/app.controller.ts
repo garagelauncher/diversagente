55bc399719165a6f3bcf5eec9444617ff6eaa41f
João Bispo <joaobispo2077@gmail.com>
2022-04-10 17:12:31 -0300

    chore: add conventional commit config

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -6,7 +6,9 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(): object {
+    return {
+      message: 'Hello World!',
+    };
   }
 }
