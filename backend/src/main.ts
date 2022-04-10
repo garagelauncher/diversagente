feaa2617e3186224ced369d5e0eefc27d7ee278b
João Bispo <joaobispo2077@gmail.com>
2022-04-10 18:27:37 -0300

    feat: add OPEN API specification

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const documentationConfig = new DocumentBuilder()
+    .setTitle('API documentation')
+    .setDescription('The API endpoints')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, documentationConfig);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
