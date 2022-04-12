eb94f94d6944eb1c1aad3cf74c867e3303253436
João Bispo <joaobispo2077@gmail.com>
2022-04-12 15:51:38 -0300

    feat: add user api example

--- backend/src/resources/users/dto/create-user.dto.ts
+++ backend/src/resources/users/dto/create-user.dto.ts
@@ -1,5 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateUserDto {
   id: string;
+
+  @ApiProperty({
+    example: 'john.doe',
+    description: 'The username of the user',
+  })
   username: string;
+  @ApiProperty({
+    example: 'john_doe@gmail.com',
+    description: 'The email of the user',
+  })
   email: string;
 }
