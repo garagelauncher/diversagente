f72a334c2da0a8a59800f75243c45ce11c32ba50
João Bispo <joaobispo2077@gmail.com>
2022-04-21 18:04:48 -0300

    feat: add cats CRUD

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { CatsModule } from './modules/cats/cats.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    CatsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ backend/src/modules/cats/cats.service.ts
@@ -0,0 +1,53 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/database/prisma.service';
+import { CreateCatDto } from './dto/create-cat.dto';
+import { UpdateCatDto } from './dto/update-cat.dto';
+import crypto from 'crypto';
+
+@Injectable()
+export class CatsService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(createCatDto: CreateCatDto) {
+    const createdCat = await this.prisma.cat.create({
+      data: {
+        ...createCatDto,
+        id: crypto.randomUUID(),
+      },
+    });
+
+    return createdCat;
+  }
+
+  async findAll() {
+    return await this.prisma.cat.findMany({});
+  }
+
+  async findOne(id: string) {
+    return await this.prisma.cat.findUnique({
+      where: {
+        id,
+      },
+    });
+  }
+
+  async update(id: string, updateCatDto: UpdateCatDto) {
+    return await this.prisma.cat.update({
+      where: {
+        id,
+      },
+      data: {
+        ...updateCatDto,
+      },
+    });
+  }
+
+  async remove(id: string) {
+    return await this.prisma.cat.delete({
+      where: {
+        id,
+      },
+    });
+  }
+}
