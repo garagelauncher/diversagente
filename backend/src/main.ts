9dad472cb44f89f2d553c87ab9281991b1ccccd6
João Bispo <joaobispo2077@gmail.com>
2022-04-21 16:43:18 -0300

    feat: add dynamic APP PORT

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const configService = app.get(ConfigService);
 
   const documentationConfig = new DocumentBuilder()
     .setTitle('API documentation')
@@ -14,6 +17,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, documentationConfig);
   SwaggerModule.setup('docs', app, document);
 
-  await app.listen(3000);
+  const PORT = configService.get<number>('PORT') || 3000;
+  await app.listen(PORT);
 }
 bootstrap();
