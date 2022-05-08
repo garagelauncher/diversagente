bc4b1ec36b010c3fa80e96aff65de75ffd754b38
João Bispo <joaobispo2077@gmail.com>
2022-05-08 16:55:58 -0300

    feat: add path mapping

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -1,6 +1,20 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
+
   return {
-    presets: ['babel-preset-expo']
+    presets: ['babel-preset-expo'],
+    plugins: [
+      ["module-resolver", {
+        root: ["./src"],
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+        alias: {
+          "@src": "./src",
+          "@components": "./src/components",
+          "@configs": "./src/configs",
+          "@hooks": "./src/hooks",
+          "@assets": "./src/assets",
+        }
+      }]
+    ]
   };
 };
