7a2c183e44e6af21e693fc14040cf286e8491fe9
João Bispo <joaobispo2077@gmail.com>
2022-05-08 17:53:57 -0300

    feat: add stack navigation

--- mobile/src/screens/Login/index.tsx
+++ mobile/src/screens/Login/index.tsx
@@ -4,6 +4,10 @@ import { Box, Button, Icon, Text } from 'native-base';
 import { TextPink } from '@components/TextPink';
 
 export const Login = () => {
+  function handleLogin() {
+    console.log('Login');
+  }
+
   return (
     <Box flex={1} justifyContent="center" padding={4}>
       <TextPink>Garage Launcher pink</TextPink>
@@ -12,7 +16,7 @@ export const Login = () => {
         colorScheme="red"
         borderRadius={4}
       >
-        <Text fontWeight={'bold'} color="white">
+        <Text fontWeight={'bold'} color="white" onPress={handleLogin}>
           Login with Google
         </Text>
       </Button>
