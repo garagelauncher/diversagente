757e81377c0873e305f9a0092c01a581b61eb4da
João Bispo <joaobispo2077@gmail.com>
2022-05-08 18:00:07 -0300

    feat: add profile page

--- mobile/src/routes/stack.routes.tsx
+++ mobile/src/routes/stack.routes.tsx
@@ -1,6 +1,7 @@
 import { createStackNavigator } from '@react-navigation/stack';
 
 import { Login } from '@src/screens/Login';
+import { Profile } from '@src/screens/Profile';
 
 const { Navigator, Screen } = createStackNavigator();
 
@@ -10,8 +11,22 @@ export const StackRoutes = () => {
       screenOptions={{
         headerShown: false,
       }}
+      initialRouteName="Login"
     >
       <Screen name="Login" component={Login} />
+    </Navigator>
+  );
+};
+
+export const StackPrivateRoutes = () => {
+  return (
+    <Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+      initialRouteName="Profile"
+    >
+      <Screen name="Profile" component={Profile} />
     </Navigator>
   );
 };
