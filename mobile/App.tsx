940e55e2f27482002cd812ab15e9f1ba586cdc8d
João Bispo <joaobispo2077@gmail.com>
2022-05-08 16:43:05 -0300

    chore: setup typescript

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Garage Launcher</Text>
+      <Text>Garage Launcher with TypesScript</Text>
       <StatusBar style="auto" />
     </View>
   );
