e6364956e1b00a8c758be636ca8fda411e7578cf
João Bispo <joaobispo2077@gmail.com>
2022-05-09 19:06:32 -0300

    feat: add User DTO body

--- backend/src/modules/users/dto/create-user.dto.ts
+++ backend/src/modules/users/dto/create-user.dto.ts
@@ -1,5 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateUserDto {
+  @ApiProperty()
   email: string;
+
+  @ApiProperty()
   username: string;
+
+  @ApiProperty()
   name: string;
 }
--- backend/src/modules/users/dto/update-user.dto.ts
+++ backend/src/modules/users/dto/update-user.dto.ts
@@ -1,4 +1,10 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateUserDto } from './create-user.dto';
+import { ApiProperty } from '@nestjs/swagger';
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @ApiProperty()
+  picture?: string;
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+  @ApiProperty()
+  bio?: string;
+}
