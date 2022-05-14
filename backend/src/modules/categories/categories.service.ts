b4504bec0f4e889f584728ac5267e08f73b9313a
vini-alm <Vinicius.Almeida.Soares@ibm.com>
2022-05-14 17:39:26 -0300

    feat: add categories CRUD

---
+++ backend/src/modules/categories/categories.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/shared/database/prisma.service';
+import { CreateCategoryDto } from './dto/create-category.dto';
+import { UpdateCategoryDto } from './dto/update-category.dto';
+
+@Injectable()
+export class CategoriesService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  create(createCategoryDto: CreateCategoryDto) {
+    return this.prisma.category.create({ data: createCategoryDto });
+  }
+
+  findAll() {
+    return this.prisma.category.findMany({});
+  }
+
+  findOne(id: string) {
+    return this.prisma.category.findUnique({ where: { id } });
+  }
+
+  update(id: string, updateCategoryDto: UpdateCategoryDto) {
+    return this.prisma.category.update({
+      where: { id },
+      data: updateCategoryDto,
+    });
+  }
+
+  remove(id: string) {
+    return this.prisma.category.delete({ where: { id } });
+  }
+}
---
+++ backend/src/modules/categories/dto/create-category.dto.ts
@@ -0,0 +1,4 @@
+export class CreateCategoryDto {
+  id:string;
+  name:string;
+}
