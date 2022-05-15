c687616ea447cb8b3c36c0681084e08786f4b443
João Bispo <joaobispo2077@gmail.com>
2022-05-15 18:56:49 -0300

    feat(wip): adding saerch by llat lon

--- backend/src/modules/locations/locations.service.ts
+++ backend/src/modules/locations/locations.service.ts
@@ -1,15 +1,54 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { PrismaService } from 'src/shared/database/prisma.service';
 import { CreateLocationDto } from './dto/create-location.dto';
 import { UpdateLocationDto } from './dto/update-location.dto';
 
 @Injectable()
 export class LocationsService {
-  create(createLocationDto: CreateLocationDto) {
-    return 'This action adds a new location';
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(createLocationDto: CreateLocationDto) {
+    return await this.prisma.location.create({
+      data: createLocationDto,
+    });
   }
 
-  findAll() {
-    return `This action returns all locations`;
+  async findAll() {
+    const distanceInKilometer = 1000 * 500; // 50km
+
+    const longitude = -46.615965;
+    const latitude = -23.5377475;
+    // create index 2dsphere into mongodb for field
+    const radius = distanceInKilometer / 6378.1;
+
+    const [quantity, locations] = await Promise.all([
+      this.prisma.location.count(),
+      this.prisma.location.findRaw({
+        filter: {
+          coordinates: {
+            $geoWithin: { $centerSphere: [[longitude, latitude], radius] },
+          },
+        },
+      }),
+      this.prisma.location.findRaw({
+        filter: {
+          coordinates: {
+            $nearSphere: {
+              $geometry: {
+                type: 'Point',
+                coordinates: [longitude, latitude],
+              },
+
+              $maxDistance: distanceInKilometer,
+            },
+          },
+        },
+      }),
+    ]);
+
+    Logger.debug(locations);
+    Logger.debug(quantity);
+    return locations;
   }
 
   findOne(id: number) {
