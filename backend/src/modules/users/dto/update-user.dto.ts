9a4529080d0836c2c803c73d70acfd537e5fd88c
João Bispo <joaobispo2077@gmail.com>
2022-05-15 15:42:00 -0300

    feat: add remove user

--- backend/src/modules/users/dto/update-user.dto.ts
+++ backend/src/modules/users/dto/update-user.dto.ts
@@ -1,10 +1,19 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateUserDto } from './create-user.dto';
 import { ApiProperty } from '@nestjs/swagger';
+import { UserPreference } from '../entities/user.entity';
 export class UpdateUserDto extends PartialType(CreateUserDto) {
   @ApiProperty()
   picture?: string;
 
   @ApiProperty()
-  bio?: string;
+  biograph?: string;
+
+  email?: string;
+  username?: string;
+  name?: string;
+  birthdate?: string | Date;
+  createdAt?: string | Date;
+  updatedAt?: string | Date;
+  preferences?: UserPreference;
 }
