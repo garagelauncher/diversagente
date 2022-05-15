5bc3aad2b7bf8261db1104997fc20bbd8996e7ae
João Bispo <joaobispo2077@gmail.com>
2022-05-15 19:42:47 -0300

    feat(wip): trying to add search by proximity with mongoose

--- backend/src/modules/locations/locations.module.ts
+++ backend/src/modules/locations/locations.module.ts
@@ -2,8 +2,13 @@ import { Module } from '@nestjs/common';
 import { LocationsService } from './locations.service';
 import { LocationsController } from './locations.controller';
 import { PrismaService } from 'src/shared/database/prisma.service';
+import { LocationsSchema } from 'src/shared/schemas/locations.schema';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'locations', schema: LocationsSchema }]),
+  ],
   controllers: [LocationsController],
   providers: [LocationsService, PrismaService],
 })
