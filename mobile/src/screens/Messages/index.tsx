8374cc73c7c338d898791ec552879827d373bc56
João Bispo <joaobispo2077@gmail.com>
2022-05-17 17:18:07 -0300

    refactor: add working in progress labels

--- mobile/src/screens/Messages/index.tsx
+++ mobile/src/screens/Messages/index.tsx
@@ -1,9 +1,16 @@
-import { Box, Heading } from 'native-base';
+import { Box, Heading, Text } from 'native-base';
 
 export const Messages = () => {
   return (
-    <Box width="100%" backgroundColor="blue.200">
+    <Box
+      width="100%"
+      backgroundColor="blue.200"
+      flex={1}
+      alignItems="center"
+      justifyContent="center"
+    >
       <Heading>MESSAGES PAGE</Heading>
+      <Text>Work In progress...</Text>
     </Box>
   );
 };
