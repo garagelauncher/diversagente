12b3dc174133248037dfe61f14c0f3b7899071b4
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2022-05-19 15:24:13 -0300

    feat: create global folder with primary colors

---
+++ mobile/global/styles/theme.ts
@@ -0,0 +1,9 @@
+export const theme = {
+  colors: {
+    orangePrimary:'#FA9600',
+    yellowPrimary:'#FFDB11',
+    pastelPrimary:'#FFF1A4',
+    bluePrimary:'#36A9E1',
+    navyPrimary :'#004AAD',
+  }
+};
