89eeae9051d33779876f6d01cbdedfe71fafad63
vini-alm <Vinicius.Almeida.Soares@ibm.com>
2022-05-21 14:51:05 -0300

    commit Create Like

--- backend/src/modules/likes/dto/create-like.dto.ts
+++ backend/src/modules/likes/dto/create-like.dto.ts
@@ -1,1 +1,9 @@
-export class CreateLikeDto {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateLikeDto {
+  @ApiProperty()
+  ownerId: string;
+
+  @ApiProperty()
+  postId: string;
+}
--- backend/src/modules/likes/likes.controller.ts
+++ backend/src/modules/likes/likes.controller.ts
@@ -1,15 +1,26 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { LikesService } from './likes.service';
 import { CreateLikeDto } from './dto/create-like.dto';
 import { UpdateLikeDto } from './dto/update-like.dto';
 
-@Controller('likes')
+@Controller('posts/:postId/likes')
 export class LikesController {
   constructor(private readonly likesService: LikesService) {}
 
   @Post()
-  create(@Body() createLikeDto: CreateLikeDto) {
-    return this.likesService.create(createLikeDto);
+  create(
+    @Param('postId') postId: string,
+    @Body() createLikeDto: CreateLikeDto,
+  ) {
+    return this.likesService.create({ postId, ...createLikeDto });
   }
 
   @Get()
@@ -19,16 +30,16 @@ export class LikesController {
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.likesService.findOne(+id);
+    return this.likesService.findOne(id);
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateLikeDto: UpdateLikeDto) {
-    return this.likesService.update(+id, updateLikeDto);
+    return this.likesService.update(id, updateLikeDto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.likesService.remove(+id);
+    return this.likesService.remove(id);
   }
 }
