549d9c7cecd3bd8733b07561a4d6322a83b4a6e2
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2022-05-21 17:58:39 -0300

    style: update background colors

--- mobile/src/screens/Forums/index.tsx
+++ mobile/src/screens/Forums/index.tsx
@@ -1,11 +1,13 @@
 import { Box } from 'native-base';
 
+import { theme } from '../../styles/theme';
+
 import { Category } from '@src/components/Category';
 import { UserAvatar } from '@src/components/UserAvatar';
 
 export const Forums = () => {
   return (
-    <Box backgroundColor={'#FFFFFF'} flex={1}>
+    <Box backgroundColor={theme.colors.lightGray} flex={1}>
       <UserAvatar picture={'https://github.com/bertiGrazi.png'} />
       <Category />
     </Box>
