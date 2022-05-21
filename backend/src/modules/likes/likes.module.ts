e518d645b1d6b3ea1a228d73753be3b8ba334c43
vini-alm <Vinicius.Almeida.Soares@ibm.com>
2022-05-21 14:51:05 -0300

    commit Create Like

--- backend/src/modules/likes/likes.module.ts
+++ backend/src/modules/likes/likes.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { LikesService } from './likes.service';
 import { LikesController } from './likes.controller';
+import { PrismaService } from 'src/shared/database/prisma.service';
 
 @Module({
   controllers: [LikesController],
-  providers: [LikesService]
+  providers: [LikesService, PrismaService],
 })
 export class LikesModule {}
