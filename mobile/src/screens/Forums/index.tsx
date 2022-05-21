4c7568d9b66f0fae1e912c363e73496bc836f666
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2022-05-21 16:44:43 -0300

    feat: create userAvatar component

--- mobile/src/screens/Forums/index.tsx
+++ mobile/src/screens/Forums/index.tsx
@@ -1,19 +1,16 @@
 import { Sucess } from '@src/components/Sucess';
-import { Box, Heading, Text, View } from 'native-base';
+import { UserAvatar } from '@src/components/UserAvatar';
+import { Avatar, Box, Heading, Text, View, HStack } from 'native-base';
 
 import { theme } from '../../styles/theme';
 
 export const Forums = () => {
   return (
     <Box
-      width="100%"
-      backgroundColor={theme.colors.pastelPrimary}
+      backgroundColor={theme.colors.bluePrimary}
       flex={1}
-      alignItems="center"
-      justifyContent="center"
     >
-      <Heading>FORUMS PAGE</Heading>
-      <Text>Work In progress...</Text>
+      <UserAvatar picture={'https://github.com/bertiGrazi.png'}   />
     </Box>
   );
 };
