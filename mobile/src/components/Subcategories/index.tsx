9f3e81d44a01416fcf8f33982e410efd4068c8a3
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2022-05-21 22:26:00 -0300

    chore: add mock for subcategories component test

--- mobile/src/components/Subcategories/index.tsx
+++ mobile/src/components/Subcategories/index.tsx
@@ -20,33 +20,26 @@ type Props = {
 
 export function Subcategories({ titles }: Props) {
   return (
-    <Box mt={10} ml="5">
-      <ScrollView>
-        <VStack alignItems="center" justifyContent="center">
-          <HStack space={6} flexWrap="wrap" alignItems="center">
-            {titles.map((item, index) => {
-              return (
-                <Button
-                  bgColor={'gray.50'}
-                  borderRadius="md"
-                  mb={4}
-                  key={index}
-                >
-                  <Center bgColor={'gray.50'} w={120} h={120} borderRadius="xl">
-                    <Center borderRadius="md">
-                      <Button bg={'gray.300'} w={100} p={1.5}>
-                        <Text textAlign="center" color={'gray.900'}>
-                          {item.text}
-                        </Text>
-                      </Button>
-                    </Center>
+    <Box mt={10} ml="6">
+      <VStack alignItems="center" justifyContent="center">
+        <HStack space={6} flexWrap="wrap" alignItems="center">
+          {titles.map((item, index) => {
+            return (
+              <Button bgColor={'gray.50'} borderRadius="md" mb={4} key={index}>
+                <Center bgColor={'gray.50'} w={120} h={120} borderRadius="xl">
+                  <Center borderRadius="md">
+                    <Button bg={'gray.300'} w={100} p={1.5}>
+                      <Text textAlign="center" color={'gray.900'}>
+                        {item.text}
+                      </Text>
+                    </Button>
                   </Center>
-                </Button>
-              );
-            })}
-          </HStack>
-        </VStack>
-      </ScrollView>
+                </Center>
+              </Button>
+            );
+          })}
+        </HStack>
+      </VStack>
     </Box>
   );
 }
--- mobile/src/screens/Forums/index.tsx
+++ mobile/src/screens/Forums/index.tsx
@@ -1,19 +1,53 @@
-import { Sucess } from '@src/components/Sucess';
-import { Box, Heading, Text, View } from 'native-base';
+import { Box, Heading, ScrollView, Text } from 'native-base';
+import React from 'react';
 
 import { theme } from '../../styles/theme';
 
+import { Subcategories, Titles } from '@src/components/Subcategories';
+
+const titles: Titles[] = [
+  {
+    id: 1,
+    text: 'alergias',
+  },
+  {
+    id: 2,
+    text: 'terapias',
+  },
+  {
+    id: 3,
+    text: 'medicação',
+  },
+  {
+    id: 4,
+    text: 'atividades físicas',
+  },
+  {
+    id: 5,
+    text: 'sono',
+  },
+  {
+    id: 6,
+    text: 'hiperatividade',
+  },
+];
+
 export const Forums = () => {
   return (
-    <Box
-      width="100%"
-      backgroundColor={theme.colors.pastelPrimary}
-      flex={1}
-      alignItems="center"
-      justifyContent="center"
-    >
-      <Heading>FORUMS PAGE</Heading>
-      <Text>Work In progress...</Text>
-    </Box>
+    <ScrollView>
+      <Box
+        width="100%"
+        backgroundColor={theme.colors.pastelPrimary}
+        flex={1}
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Box mt={100} alignItems="center" alignContent="center">
+          <Heading>FORUMS PAGE</Heading>
+          <Text>Work In progress...</Text>
+        </Box>
+        <Subcategories titles={titles}></Subcategories>
+      </Box>
+    </ScrollView>
   );
 };
