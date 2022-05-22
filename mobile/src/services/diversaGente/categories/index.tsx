c2afef0c09faf3266567e83e001aa87d868170df
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2022-05-22 15:32:08 -0300

    feat: add methods to fetch all categories and subcategories to populate forum component

--- mobile/src/services/diversaGente/categories/index.tsx
+++ mobile/src/services/diversaGente/categories/index.tsx
@@ -6,6 +6,7 @@ export const findAllCategories = async () => {
   try {
     const response = await diversagenteBaseApi.get<Category[]>('/categories');
     const categories = response.data;
+    console.info('CATEGORIES', categories);
     return categories;
   } catch (error) {
     console.error(error);
