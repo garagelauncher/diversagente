bd8661bf03fa4af3f8fe689f53ca9de4de4dbac3
João Bispo <joaobispo2077@gmail.com>
2022-05-22 20:56:20 -0300

    feat: add go back navigation into LocationDetails page

--- mobile/src/screens/LocationDetails/index.tsx
+++ mobile/src/screens/LocationDetails/index.tsx
@@ -1,5 +1,29 @@
-import { Box, Heading, Text } from 'native-base';
+import { Feather } from '@expo/vector-icons';
+import {
+  NavigationProp,
+  RouteProp,
+  useNavigation,
+  useRoute,
+} from '@react-navigation/native';
+import { Box, Heading, Icon, IconButton, Text } from 'native-base';
+
+import { StackLocationNavigatorParamList } from '@src/routes/locationStack.routes';
+
+type LocationDetailsScreenNavigationProps = NavigationProp<
+  StackLocationNavigatorParamList,
+  'LocationDetails'
+>;
+
 export const LocationDetails = () => {
+  const navigation = useNavigation<LocationDetailsScreenNavigationProps>();
+  const route =
+    useRoute<RouteProp<StackLocationNavigatorParamList, 'LocationDetails'>>();
+  const { id } = route.params;
+
+  const handleNavigateGoBack = () => {
+    navigation.goBack();
+  };
+
   return (
     <Box
       width="100%"
@@ -8,8 +32,18 @@ export const LocationDetails = () => {
       alignItems="center"
       justifyContent="center"
     >
+      <IconButton
+        colorScheme="gray"
+        variant={'solid'}
+        icon={<Icon as={<Feather name="arrow-left" />} />}
+        onPress={handleNavigateGoBack}
+        position="absolute"
+        top={10}
+        left={10}
+      />
       <Heading>Location details PAGE</Heading>
       <Text>Work In progress...</Text>
+      <Text>{id}</Text>
     </Box>
   );
 };
