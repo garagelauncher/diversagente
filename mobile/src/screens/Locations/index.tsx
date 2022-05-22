6d509e9dd9366021efcc2a223d99e79f0d65a626
João Bispo <joaobispo2077@gmail.com>
2022-05-22 18:28:52 -0300

    feat: check user permissions before use localization

--- mobile/src/screens/Locations/index.tsx
+++ mobile/src/screens/Locations/index.tsx
@@ -1,11 +1,12 @@
 import { SimpleLineIcons } from '@expo/vector-icons';
-import { Box, Icon, Text } from 'native-base';
+import * as ExpoLocation from 'expo-location';
+import { Box, Button, Icon, Image, Popover, Text, View } from 'native-base';
 import { useCallback, useEffect, useState } from 'react';
+import { Alert, StyleSheet } from 'react-native';
 import MapView, { Marker, Region, PROVIDER_GOOGLE } from 'react-native-maps';
 
-import * as styles from './styles';
+import * as customStyles from './styles';
 
-import { Category } from '@src/contracts/Category';
 import { Location } from '@src/contracts/Location';
 import { diversaGenteServices } from '@src/services/diversaGente';
 
@@ -13,13 +14,38 @@ const LATITUDE_DELTA = 0.0922;
 const LONGITUDE_DELTA = 0.0421;
 
 export const Locations = () => {
+  const [isPopoverOpen, setIsPopoverOpen] = useState(false);
   const [locations, setLocations] = useState<Location[]>([]);
   const [initialPosition, setInitialPosition] = useState<Region | undefined>(
     undefined,
   );
-  const [categories, setCategories] = useState<Category[]>([]);
 
   console.log('locations state', locations);
+
+  const getUserCurrentLocation = useCallback(async () => {
+    const { status } = await ExpoLocation.requestForegroundPermissionsAsync();
+
+    if (status !== 'granted') {
+      Alert.alert(
+        'Você negou a permissão de localização',
+        'Precisamos de sua permissão para obter a localização.',
+      );
+      return;
+    }
+
+    const location = await ExpoLocation.getCurrentPositionAsync();
+
+    const { latitude, longitude } = location.coords;
+
+    console.debug('latitude', latitude);
+    console.debug('longitude', longitude);
+    setInitialPosition({
+      latitude,
+      longitude,
+      latitudeDelta: LATITUDE_DELTA,
+      longitudeDelta: LONGITUDE_DELTA,
+    });
+  }, []);
 
   const fetchLocations = useCallback(async () => {
     diversaGenteServices
@@ -35,10 +61,6 @@ export const Locations = () => {
         setLocations(foundLocations);
       })
       .catch((error) => console.error('deu ruim'));
-
-    const categoriesFromApi = await diversaGenteServices.findAllCategories();
-
-    setCategories(categoriesFromApi);
   }, []);
 
   useEffect(() => {
@@ -51,13 +73,14 @@ export const Locations = () => {
     console.debug('loaded focus on map');
 
     fetchLocations();
-  }, [fetchLocations]);
+    getUserCurrentLocation();
+  }, [fetchLocations, getUserCurrentLocation]);
 
   return (
     <Box flex={1}>
       <MapView
         provider={PROVIDER_GOOGLE}
-        style={styles.mapStyles}
+        style={customStyles.mapStyles}
         initialRegion={initialPosition}
       >
         {locations.map((location) => {
@@ -74,7 +97,7 @@ export const Locations = () => {
                 longitude: location.coordinates.longitude,
               }}
             >
-              <Box background={'blue.400'} borderRadius={5} minWidth={300}>
+              {/* <Box background={'blue.400'} borderRadius={5} minWidth={300}>
                 <Icon
                   as={<SimpleLineIcons name={'graduation'} color="white" />}
                   color="white"
@@ -90,6 +113,41 @@ export const Locations = () => {
                   {location.title}
                 </Text>
                 <Box style={styles.mapPicker} />
+              </Box> */}
+
+              <Box h="60%" w="100%" alignItems="center">
+                <Popover
+                  isOpen={isPopoverOpen}
+                  trigger={(triggerProps) => {
+                    return (
+                      <Button
+                        {...triggerProps}
+                        colorScheme="danger"
+                        onPress={() => setIsPopoverOpen(!isPopoverOpen)}
+                      >
+                        Delete Customer
+                      </Button>
+                    );
+                  }}
+                >
+                  <Popover.Content accessibilityLabel="Delete Customerd" w="56">
+                    <Popover.Arrow />
+                    <Popover.CloseButton />
+                    <Popover.Header>Delete Customer</Popover.Header>
+                    <Popover.Body>
+                      This will remove all data relating to Alex. This action
+                      cannot be reversed. Deleted data can not be recovered.
+                    </Popover.Body>
+                    <Popover.Footer justifyContent="flex-end">
+                      <Button.Group space={2}>
+                        <Button colorScheme="coolGray" variant="ghost">
+                          Cancel
+                        </Button>
+                        <Button colorScheme="danger">Deletei</Button>
+                      </Button.Group>
+                    </Popover.Footer>
+                  </Popover.Content>
+                </Popover>
               </Box>
             </Marker>
           );
@@ -98,3 +156,99 @@ export const Locations = () => {
     </Box>
   );
 };
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 32,
+    paddingTop: 20,
+  },
+
+  title: {
+    fontSize: 20,
+    fontFamily: 'Ubuntu_700Bold',
+    marginTop: 24,
+  },
+
+  description: {
+    color: '#6C6C80',
+    fontSize: 16,
+    marginTop: 4,
+    fontFamily: 'Roboto_400Regular',
+  },
+
+  mapContainer: {
+    flex: 1,
+    width: '100%',
+    borderRadius: 10,
+    overflow: 'hidden',
+    marginTop: 16,
+  },
+
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+
+  mapMarker: {
+    width: 300,
+    height: 150,
+  },
+
+  mapMarkerContainer: {
+    width: 90,
+    height: 70,
+    backgroundColor: '#34CB79',
+    flexDirection: 'column',
+    borderRadius: 8,
+    overflow: 'hidden',
+    alignItems: 'center',
+  },
+
+  mapMarkerImage: {
+    width: 90,
+    height: 45,
+    resizeMode: 'cover',
+  },
+
+  mapMarkerTitle: {
+    flex: 1,
+    fontFamily: 'Roboto_400Regular',
+    color: '#FFF',
+    fontSize: 13,
+    lineHeight: 23,
+  },
+
+  itemsContainer: {
+    flexDirection: 'row',
+    marginTop: 16,
+    marginBottom: 32,
+  },
+
+  item: {
+    backgroundColor: '#fff',
+    borderWidth: 2,
+    borderColor: '#eee',
+    height: 120,
+    width: 120,
+    borderRadius: 8,
+    paddingHorizontal: 16,
+    paddingTop: 20,
+    paddingBottom: 16,
+    marginRight: 8,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+
+    textAlign: 'center',
+  },
+
+  selectedItem: {
+    borderColor: '#34CB79',
+    borderWidth: 2,
+  },
+
+  itemTitle: {
+    fontFamily: 'Roboto_400Regular',
+    textAlign: 'center',
+    fontSize: 13,
+  },
+});
