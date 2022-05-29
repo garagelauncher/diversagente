d449c85eda511be1d7e1ed137cdde1083bc97948
João Bispo <joaobispo2077@gmail.com>
2022-05-29 17:10:12 -0300

    feat: improve layout of location details

--- mobile/src/utils/formatDate.ts
+++ mobile/src/utils/formatDate.ts
@@ -1,3 +1,10 @@
-export const formatDate = (date: string) => {
-  return new Intl.DateTimeFormat('pt-BR').format(new Date(date));
+export const formatDate = (date: string | undefined) => {
+  if (date) {
+    return new Intl.DateTimeFormat('pt-BR', {
+      year: 'numeric',
+      month: 'long',
+      day: '2-digit',
+    }).format(new Date(date));
+  }
+  return date;
 };
