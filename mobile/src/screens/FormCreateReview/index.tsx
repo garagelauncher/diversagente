b486f550eff5f2459365edf9bba87cdbea0fd8a7
João Bispo <joaobispo2077@gmail.com>
2022-05-29 16:08:22 -0300

    feat: add routing from location details to reviews & create review

---
+++ mobile/src/screens/FormCreateReview/index.tsx
@@ -0,0 +1,16 @@
+import { Box, Heading, Text } from 'native-base';
+
+export const FormCreateReview = () => {
+  return (
+    <Box
+      width="100%"
+      backgroundColor="orange.200"
+      flex={1}
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Heading>FormCreateReview PAGE</Heading>
+      <Text>Work In progress...</Text>
+    </Box>
+  );
+};
