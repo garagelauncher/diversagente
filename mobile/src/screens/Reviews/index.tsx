5b83f6650b249da33d64e8edbc045536649c9e8b
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2022-05-29 18:18:00 -0300

    wip: add logic to get all reviews from a location to show in component

--- mobile/src/screens/Reviews/index.tsx
+++ mobile/src/screens/Reviews/index.tsx
@@ -1,16 +1,81 @@
-import { Box, Heading, Text } from 'native-base';
+import { Feather } from '@expo/vector-icons';
+import {
+  useNavigation,
+  useRoute,
+  RouteProp,
+  NavigationProp,
+} from '@react-navigation/native';
+import { Review } from '@src/contracts/Reviews';
+import { Box, HStack, Icon, IconButton } from 'native-base';
+import React, { useCallback, useEffect, useState } from 'react';
+
+import { StackLocationNavigatorParamList } from '@src/routes/locationStack.routes';
+import { diversaGenteServices } from '@src/services/diversaGente';
+
+type ReviewsScreenNavigationProps = NavigationProp<
+  StackLocationNavigatorParamList,
+  'Reviews'
+>;
 
 export const Reviews = () => {
+  const [reviews, setReviews] = useState<Review[] | null>([]);
+
+  const navigation = useNavigation<ReviewsScreenNavigationProps>();
+  const route =
+    useRoute<RouteProp<StackLocationNavigatorParamList, 'Reviews'>>();
+  const { locationId } = route.params;
+
+  const handleNavigateGoBack = () => {
+    navigation.goBack();
+  };
+
+  const fetchAllReviewsFromLocation = useCallback(
+    async (locationId: string) => {
+      try {
+        const reviewsFromApi =
+          await diversaGenteServices.getReviewsByLocationId(locationId);
+        setReviews(reviewsFromApi);
+      } catch (error) {
+        console.info('Error while fetching all reviews', error);
+      }
+    },
+    [],
+  );
+
+  useEffect(() => {
+    console.log('id in use effect', locationId);
+    fetchAllReviewsFromLocation(locationId);
+  }, [fetchAllReviewsFromLocation, locationId]);
+
   return (
-    <Box
-      width="100%"
-      backgroundColor="pink.200"
-      flex={1}
-      alignItems="center"
-      justifyContent="center"
-    >
-      <Heading>Reviews PAGE</Heading>
-      <Text>Work In progress...</Text>
-    </Box>
+    <HStack>
+      <IconButton
+        colorScheme="gray"
+        variant={'solid'}
+        icon={<Icon as={<Feather name="arrow-left" />} />}
+        onPress={handleNavigateGoBack}
+        position="absolute"
+        top={10}
+        left={10}
+        zIndex={1}
+      />
+      {reviews?.map((item, index) => {
+        return (
+          <Box
+            key={index}
+            size="md"
+            _text={{
+              color: 'amber.50',
+            }}
+            marginTop={2}
+            marginLeft={2}
+            w={140}
+            bg={'darkBlue.700'}
+          >
+            {item.text}
+          </Box>
+        );
+      })}
+    </HStack>
   );
 };
