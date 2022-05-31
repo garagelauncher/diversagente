dbd30b8313d48623856d418728dc72bc9206e5e6
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2022-05-30 21:51:10 -0300

    wip: create review forms

--- mobile/src/screens/FormCreateReview/index.tsx
+++ mobile/src/screens/FormCreateReview/index.tsx
@@ -1,4 +1,4 @@
-import { Feather, Fontisto } from '@expo/vector-icons';
+import { Feather } from '@expo/vector-icons';
 import {
   useNavigation,
   useRoute,
@@ -6,146 +6,117 @@ import {
   NavigationProp,
 } from '@react-navigation/native';
 import {
-  Avatar,
   Box,
-  Divider,
+  FormControl,
   Heading,
-  HStack,
   Icon,
   IconButton,
-  ScrollView,
   Text,
+  Stack,
+  Button,
+  TextArea,
   VStack,
+  ScrollView,
 } from 'native-base';
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 import { AirbnbRating } from 'react-native-ratings';
 
-import { Review } from '@src/contracts/Review';
 import { StackLocationNavigatorParamList } from '@src/routes/locationStack.routes';
-import { diversaGenteServices } from '@src/services/diversaGente';
-import { formatDate } from '@src/utils/formatDate';
 
-type ReviewsScreenNavigationProps = NavigationProp<
+type FormCreateReviewScreenNavigationProps = NavigationProp<
   StackLocationNavigatorParamList,
-  'Reviews'
+  'FormCreateReview'
 >;
 
-export const Reviews = () => {
-  const [reviews, setReviews] = useState<Review[] | null>([]);
+export const FormCreateReview = () => {
+  const [reviewText, setReviewText] = useState('');
+  const [reviewRate, setReviewRate] = useState(0);
 
-  const navigation = useNavigation<ReviewsScreenNavigationProps>();
+  const navigation = useNavigation<FormCreateReviewScreenNavigationProps>();
   const route =
-    useRoute<RouteProp<StackLocationNavigatorParamList, 'Reviews'>>();
+    useRoute<RouteProp<StackLocationNavigatorParamList, 'FormCreateReview'>>();
   const { locationId } = route.params;
 
   const handleNavigateGoBack = () => {
     navigation.goBack();
   };
 
+  const statusBarHeight = getStatusBarHeight();
   const ratingCompleted = (rating: number) => {
     console.log('Rating is: ' + rating);
+    setReviewRate(rating);
+  };
+  const textValue = (text: string) => {
+    console.log(text);
+    setReviewText(text);
   };
 
-  const fetchAllReviewsFromLocation = useCallback(
-    async (locationId: string) => {
-      try {
-        const reviewsFromApi =
-          await diversaGenteServices.getReviewsByLocationId(locationId);
-        setReviews(reviewsFromApi);
-      } catch (error) {
-        console.info('Error while fetching all reviews', error);
-      }
-    },
-    [],
-  );
-
-  useEffect(() => {
-    fetchAllReviewsFromLocation(locationId);
-  }, [fetchAllReviewsFromLocation, locationId]);
-
-  const statusBarHeight = getStatusBarHeight();
+  const handleSubmit = () => {
+    console.log(reviewText);
+    console.log(reviewRate);
+  };
 
   return (
-    <VStack p={4} flex={1} mt={statusBarHeight}>
-      <IconButton
-        colorScheme="gray"
-        variant={'solid'}
-        icon={<Icon as={<Feather name="arrow-left" />} />}
-        onPress={handleNavigateGoBack}
-        position="absolute"
-        top={6}
-        ml={4}
-        mb={2}
-        zIndex={1}
-      />
-      <Heading fontSize={30} alignSelf={'center'} mt={3}>
-        Reviews
-      </Heading>
-
-      <ScrollView mt={6}>
-        {reviews?.map((item, index) => {
-          return (
-            <Box
-              key={index}
-              size="md"
-              borderRadius={20}
-              _text={{
-                color: 'amber.50',
-              }}
-              w={'100%'}
-              h={'auto'}
+    <Box w="100%" flex={1} justifyContent="center" mt={statusBarHeight}>
+      <VStack p={4}>
+        <IconButton
+          colorScheme="gray"
+          variant={'solid'}
+          icon={<Icon as={<Feather name="arrow-left" />} />}
+          onPress={handleNavigateGoBack}
+          position="absolute"
+          top={6}
+          ml={4}
+          mb={2}
+          zIndex={1}
+        />
+        <Heading fontSize={30} alignSelf={'center'} mt={3} mb={10}>
+          Avaliar local
+        </Heading>
+      </VStack>
+      <ScrollView>
+        <VStack>
+          <FormControl px={9}>
+            <Stack space={5}>
+              <Stack justifyContent={'initial'} w={'100%'}>
+                <FormControl.Label mb={5}>
+                  <Text fontSize={22}> Como foi a sua experiência? </Text>
+                </FormControl.Label>
+                <TextArea
+                  bg={'warning.50'}
+                  p={5}
+                  h={200}
+                  fontSize={16}
+                  placeholder="Conte aqui!"
+                  maxW="100%"
+                  autoCompleteType={undefined}
+                  onChangeText={textValue}
+                />
+              </Stack>
+              <Stack mt={5}>
+                <FormControl.Label mb={5}>
+                  <Text fontSize={22}>Dê uma nota: </Text>
+                </FormControl.Label>
+                <AirbnbRating
+                  defaultRating={0}
+                  selectedColor="#0284c7"
+                  showRating={false}
+                  onFinishRating={ratingCompleted}
+                />
+              </Stack>
+            </Stack>
+            <Button
               mt={10}
-              px={4}
-              py={8}
-              bg={'warning.300'}
+              bg={'warning.600'}
+              onPress={handleSubmit}
+              colorScheme={'orange'}
             >
-              <Fontisto name="quote-a-right" size={28} color="black" />
-              <Text mb={5} mt={4} fontSize={20}>
-                {item.text}
-                lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              </Text>
-              <Divider my={5} bg={'muted.800'} />
-              <HStack
-                bottom={0}
-                alignItems="center"
-                justifyContent="space-evenly"
-              >
-                <HStack alignItems="center">
-                  {item.owner.picture ? (
-                    <Avatar
-                      size="md"
-                      source={{
-                        uri: item.owner.picture,
-                      }}
-                    ></Avatar>
-                  ) : (
-                    <Avatar size="md" bg={'info.600'}></Avatar>
-                  )}
-
-                  <VStack pl={2} pr={5}>
-                    <Text fontSize={16} color={'muted.800'}>
-                      {item.owner.name}
-                    </Text>
-                    <Text fontSize={16} color={'muted.500'}>
-                      {formatDate(item.updatedAt)}
-                    </Text>
-                  </VStack>
-                </HStack>
-                <Box>
-                  <AirbnbRating
-                    selectedColor="#0284c7"
-                    size={22}
-                    defaultRating={item.stars}
-                    showRating={false}
-                    onFinishRating={ratingCompleted}
-                  />
-                </Box>
-              </HStack>
-            </Box>
-          );
-        })}
+              <Text fontSize={20}>Enviar avaliação</Text>
+            </Button>
+          </FormControl>
+        </VStack>
       </ScrollView>
-    </VStack>
+    </Box>
   );
 };
