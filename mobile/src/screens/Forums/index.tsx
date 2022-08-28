e233ba6c88bfc878439a37fe1ace1a93e3e9a42d
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2022-08-28 10:21:30 -0300

    fix header component and create box white

--- mobile/src/screens/Forums/index.tsx
+++ mobile/src/screens/Forums/index.tsx
@@ -1,12 +1,25 @@
 /* eslint-disable import/no-unresolved */
 import { Header } from '@src/components/Header/index';
-import { Box, Heading, ScrollView, Text } from 'native-base';
+import { theme } from '@src/styles/theme';
+import { Box } from 'native-base';
 
 export const Forums = () => {
   return (
-    <Header
-      title={'Olá, Katarina'}
-      description={'Encontre as categorias de seu interesse'}
-    ></Header>
+    <>
+      <Header
+        title={'Olá, Katarina'}
+        description={'Encontre mais categorias de seu interesse'}
+      ></Header>
+      <Box
+        width="100%"
+        flex={1}
+        marginTop={-3}
+        backgroundColor={theme.colors.primaryColor}
+        alignItems="center"
+        justifyContent="center"
+        borderTopLeftRadius={14}
+        borderTopRightRadius={14}
+      ></Box>
+    </>
   );
 };
