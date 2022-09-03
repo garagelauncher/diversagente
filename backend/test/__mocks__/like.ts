0d5ef75a54e94cb6cc5e1319b5c7f2d20161eeef
Gabriel Ruiz <gmruiz2009@gmail.com>
2022-09-03 16:40:50 -0300

    trying to return a like creation error

--- backend/test/__mocks__/like.ts
+++ backend/test/__mocks__/like.ts
@@ -7,3 +7,11 @@ export const likeMock = {
     createdAt: '2022-01-01T01:01:01.000Z',
     deletedAt: '2022-01-01T01:01:01.000Z',
 }
+
+export const deletedlikeMock = {
+    id: 'oioioioi-00000-oioioi-000000',
+    ownerID: '',
+    postID: '',
+    createdAt: '2022-01-01T01:01:01.000Z',
+    deletedAt: '2022-01-01T01:01:01.000Z',
+}
--- backend/test/modules/likes/likes.service.spec.ts
+++ backend/test/modules/likes/likes.service.spec.ts
@@ -3,6 +3,7 @@ import { LikesService } from './../../../src/modules/likes/likes.service';
 import { PrismaService } from './../../../src/shared/database/prisma.service';
 import { likeMock } from 'test/__mocks__/like';
 import { createPrismaProviderMock } from './../../__mocks__/prisma';
+import { CreateLikeDto } from 'src/modules/likes/dto/create-like.dto';
 
 
 describe('LikesService', () => {
@@ -19,20 +20,31 @@ describe('LikesService', () => {
 
         prisma.like.create = jest.fn().mockResolvedValue(likeMock);
         prisma.like.delete = jest.fn().mockResolvedValue(likeMock);
-})
-
-it('it should be able to create a like in a post', () => {
-    const like = {
-        ownerID: '1',
-        postID: '1',
-    }
 });
+
+it('should be defined', () => {
+    expect(likeService).toBeDefined();
+  });
+
+it('it should be able to create a like in a post with success', async () => {
+    const like = {
+        ownerId: '1',
+        postId: '1',
+    }
+    const createdLike = await likeService.create(like);
+    expect(createdLike).toEqual(likeMock);
+});
+
+// it('should not be able to create a like in a post with success', async () => {
+//     prisma.like.create = jest.fn().mockResolvedValue(undefined)
+
+//     expect(likeService.create()).rejects.toThrowError('Não deu.');
+// });
 
 it('should be able to delete a like from a post', async () => {
     const deletedLike = await likeService.remove(
         'pipipipipi-00000-popopopo-000000'
     );
     expect(deletedLike).toEqual(likeMock);
-})
-
-})
+});
+});
