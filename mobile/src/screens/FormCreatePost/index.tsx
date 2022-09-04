d30dabeb87385b0e45653700dd56ab80e22432ab
vivianequeiroz <vivianedsqueiroz@gmail.com>
2022-09-04 20:41:21 -0300

    wip: post creation skeleton

--- mobile/src/screens/FormCreatePost/index.tsx
+++ mobile/src/screens/FormCreatePost/index.tsx
@@ -1,3 +1,4 @@
+import { Ionicons } from '@expo/vector-icons';
 import {
   VStack,
   Button,
@@ -13,10 +14,22 @@ import {
   TextField,
   Text,
   Divider,
+  Badge,
+  Link,
+  WarningOutlineIcon,
+  Input,
+  Icon,
 } from 'native-base';
-import React from 'react';
 import { useForm, Controller } from 'react-hook-form';
+import {
+  Keyboard,
+  KeyboardAvoidingView,
+  ScrollView,
+  TouchableWithoutFeedback,
+} from 'react-native';
 
+import { Categories } from '@src/components/Categories';
+import { Category } from '@src/contracts/Category';
 import { logger } from '@src/utils/logger';
 
 export const FormCreatePost = () => {
@@ -32,71 +45,172 @@ export const FormCreatePost = () => {
   };
 
   return (
-    <VStack space={4} marginTop="24" padding="8">
-      <HStack alignContent={'center'} justifyContent="space-between">
-        <Heading>Nova postagem</Heading>
-        <CloseIcon marginTop="2" />
-      </HStack>
-      <Divider
-        my="2"
-        _light={{
-          bg: 'muted.300',
-        }}
-        _dark={{
-          bg: 'muted.50',
-        }}
-      />
-      <Center>
-        <Alert maxW="400" status="warning" colorScheme="orange">
-          <VStack space={2} flexShrink={1} w="100%">
-            <HStack
-              flexShrink={1}
-              space={2}
-              alignItems="center"
-              justifyContent="space-between"
-            >
-              <HStack flexShrink={1} space={2} alignItems="center">
-                <Alert.Icon />
-                <Text>Antes de criar postagens</Text>
-              </HStack>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <KeyboardAvoidingView behavior="position" enabled>
+        <ScrollView>
+          <VStack space={4} marginTop="24" padding="8">
+            <HStack alignContent={'center'} justifyContent="space-between">
+              <Heading>Nova postagem</Heading>
+              <CloseIcon marginTop="2" />
             </HStack>
-            <Box
-              pl="6"
-              _text={{
-                color: 'coolGray.600',
+            <Divider
+              my="2"
+              _light={{
+                bg: 'muted.300',
               }}
-            >
-              <Text>
-                We are happy to announce that we are going live on July 28th.
-                Get ready!
-              </Text>
-            </Box>
-          </VStack>
-        </Alert>
-      </Center>
-      <FormControl isRequired isInvalid={'thought' in errors}>
-        <FormControl.Label>What do you think?</FormControl.Label>
-        <Controller
-          control={control}
-          render={({ field: { onChange, value } }) => (
-            <TextArea
-              placeholder="TextArea"
-              onChangeText={(val) => onChange(val)}
-              defaultValue={value}
-              autoCompleteType="off"
+              _dark={{
+                bg: 'muted.50',
+              }}
             />
-          )}
-          name="thought"
-          rules={{ required: 'Field is required', minLength: 3 }}
-          defaultValue="I love NativeBase."
-        />
-        <FormControl.ErrorMessage>
-          {errors.thought?.message}
-        </FormControl.ErrorMessage>
-      </FormControl>
-      <Button onPress={handleSubmit(onSubmit)} colorScheme="pink">
-        Submit
-      </Button>
-    </VStack>
+
+            <Alert maxW="400" status="warning" colorScheme="orange">
+              <VStack space={2} flexShrink={1} w="100%">
+                <HStack
+                  flexShrink={1}
+                  space={2}
+                  alignItems="center"
+                  justifyContent="space-between"
+                >
+                  <HStack flexShrink={1} space={2} alignItems="center">
+                    <Alert.Icon />
+                    <Text fontSize="16" fontWeight="medium">
+                      Antes de criar postagens
+                    </Text>
+                  </HStack>
+                  <IconButton
+                    variant="unstyled"
+                    _focus={{
+                      borderWidth: 0,
+                    }}
+                    icon={<CloseIcon size="3" />}
+                    _icon={{
+                      color: 'coolGray.600',
+                    }}
+                  />
+                </HStack>
+                <Box
+                  pl="6"
+                  _text={{
+                    color: 'coolGray.600',
+                  }}
+                >
+                  <Text>
+                    Lembre-se de ser respeitoso e de que essa comunidade é
+                    voltada para ajudar pais, familiares e amigos de crianças
+                    neurodiversas.
+                  </Text>
+                </Box>
+              </VStack>
+            </Alert>
+
+            <VStack space="6">
+              <HStack
+                flexShrink={1}
+                space={2}
+                alignItems="center"
+                justifyContent="space-between"
+              >
+                <Button size="md" variant="outline">
+                  Selecionar categoria
+                </Button>
+                <Text>Saúde</Text>
+              </HStack>
+              <HStack
+                flexShrink={1}
+                space={2}
+                alignItems="center"
+                justifyContent="space-between"
+              >
+                <Button size="md" variant="outline">
+                  Selecionar subcategoria
+                </Button>
+                <Text>Saúde mental</Text>
+              </HStack>
+            </VStack>
+
+            <FormControl isRequired isInvalid={false}>
+              <FormControl.Label>
+                <Text fontSize="16" fontWeight="bold" marginBottom="2">
+                  Título
+                </Text>
+              </FormControl.Label>
+              <Input size="md" placeholder="Máximo de 40 caracteres" />
+              <FormControl.ErrorMessage
+                leftIcon={<WarningOutlineIcon size="s" />}
+              >
+                O título é obrigatório para conseguir fazer postagens.
+              </FormControl.ErrorMessage>
+            </FormControl>
+
+            <FormControl
+              isRequired
+              isInvalid={'thought' in errors}
+              marginBottom="2"
+            >
+              <Text fontSize="16" fontWeight="bold" marginBottom="2">
+                Imagem
+              </Text>
+              <HStack justifyContent="space-between" alignItems="center">
+                <Text>Formatos aceitos: png e jpg.</Text>
+                <Button
+                  width="32"
+                  leftIcon={
+                    <Icon as={Ionicons} name="cloud-upload-outline" size="sm" />
+                  }
+                >
+                  Upload
+                </Button>
+              </HStack>
+              <FormControl.Label>
+                <Text fontSize="16" fontWeight="bold" marginBottom="2">
+                  Conteúdo
+                </Text>
+              </FormControl.Label>
+              <Controller
+                control={control}
+                render={({ field: { onChange, value } }) => (
+                  <ScrollView>
+                    <TextArea
+                      size="md"
+                      width="100%"
+                      placeholder="Máximo de 1800 caracteres."
+                      onChangeText={(val) => onChange(val)}
+                      defaultValue={value}
+                      autoCompleteType="off"
+                    />
+                  </ScrollView>
+                )}
+                name="thought"
+                rules={{
+                  required: 'O conteúdo da postagem é obrigatório',
+                  minLength: 3,
+                }}
+              />
+              <FormControl.ErrorMessage>
+                {errors.thought?.message}
+              </FormControl.ErrorMessage>
+            </FormControl>
+            <HStack width="100%" justifyContent="space-between">
+              <Button
+                width="40"
+                onPress={handleSubmit(onSubmit)}
+                colorScheme="blue"
+                variant="outline"
+                borderColor="blue.500"
+              >
+                Cancelar
+              </Button>
+              <Button
+                width="40"
+                onPress={handleSubmit(onSubmit)}
+                colorScheme="blue"
+              >
+                Criar
+              </Button>
+            </HStack>
+          </VStack>
+        </ScrollView>
+      </KeyboardAvoidingView>
+    </TouchableWithoutFeedback>
   );
 };
