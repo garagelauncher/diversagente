e3ff0478e96903b6dae1b30ebe4788ec383ab53b
vivianequeiroz <vivianedsqueiroz@gmail.com>
2022-09-04 18:59:16 -0300

    wip: post creation component base structure

--- mobile/src/screens/FormCreatePost/index.tsx
+++ mobile/src/screens/FormCreatePost/index.tsx
@@ -1,4 +1,20 @@
-import { VStack, Button, FormControl, TextArea } from 'native-base';
+import {
+  VStack,
+  Button,
+  FormControl,
+  TextArea,
+  Heading,
+  HStack,
+  Alert,
+  Box,
+  Center,
+  CloseIcon,
+  IconButton,
+  TextField,
+  Text,
+  Divider,
+} from 'native-base';
+import React from 'react';
 import { useForm, Controller } from 'react-hook-form';
 
 import { logger } from '@src/utils/logger';
@@ -16,7 +32,48 @@ export const FormCreatePost = () => {
   };
 
   return (
-    <VStack width="80%" space={4}>
+    <VStack space={4} marginTop="24" padding="8">
+      <HStack alignContent={'center'} justifyContent="space-between">
+        <Heading>Nova postagem</Heading>
+        <CloseIcon marginTop="2" />
+      </HStack>
+      <Divider
+        my="2"
+        _light={{
+          bg: 'muted.300',
+        }}
+        _dark={{
+          bg: 'muted.50',
+        }}
+      />
+      <Center>
+        <Alert maxW="400" status="warning" colorScheme="orange">
+          <VStack space={2} flexShrink={1} w="100%">
+            <HStack
+              flexShrink={1}
+              space={2}
+              alignItems="center"
+              justifyContent="space-between"
+            >
+              <HStack flexShrink={1} space={2} alignItems="center">
+                <Alert.Icon />
+                <Text>Antes de criar postagens</Text>
+              </HStack>
+            </HStack>
+            <Box
+              pl="6"
+              _text={{
+                color: 'coolGray.600',
+              }}
+            >
+              <Text>
+                We are happy to announce that we are going live on July 28th.
+                Get ready!
+              </Text>
+            </Box>
+          </VStack>
+        </Alert>
+      </Center>
       <FormControl isRequired isInvalid={'thought' in errors}>
         <FormControl.Label>What do you think?</FormControl.Label>
         <Controller
