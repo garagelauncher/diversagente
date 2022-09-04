2a668e26907e72c9c6c933ef0f69aebfc76f10a9
João Bispo <joaobispo2077@gmail.com>
2022-09-04 17:58:47 -0300

    test: should not be able to findOne like that doesnt exist

--- backend/test/modules/likes/likes.service.spec.ts
+++ backend/test/modules/likes/likes.service.spec.ts
@@ -3,54 +3,50 @@ import { LikesService } from './../../../src/modules/likes/likes.service';
 import { PrismaService } from './../../../src/shared/database/prisma.service';
 import { likeMock } from 'test/__mocks__/like';
 import { createPrismaProviderMock } from './../../__mocks__/prisma';
-import { CreateLikeDto } from 'src/modules/likes/dto/create-like.dto';
-
 
 describe('LikesService', () => {
-    let likeService: LikesService;
-    let prisma: PrismaService;
+  let likeService: LikesService;
+  let prisma: PrismaService;
 
-    beforeEach(async () => {
-        const module: TestingModule = await Test.createTestingModule({
-            providers: [LikesService, createPrismaProviderMock()],
-        }).compile();
-            
-        likeService = module.get<LikesService>(LikesService);
-        prisma = module.get<PrismaService>(PrismaService);
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [LikesService, createPrismaProviderMock()],
+    }).compile();
 
-        prisma.like.create = jest.fn().mockResolvedValue(likeMock);
-        prisma.like.delete = jest.fn().mockResolvedValue(likeMock);
-});
+    likeService = module.get<LikesService>(LikesService);
+    prisma = module.get<PrismaService>(PrismaService);
 
-it('should be defined', () => {
+    prisma.like.create = jest.fn().mockResolvedValue(likeMock);
+    prisma.like.delete = jest.fn().mockResolvedValue(likeMock);
+  });
+
+  it('should be defined', () => {
     expect(likeService).toBeDefined();
   });
 
-it('it should be able to create a like in a post with success', async () => {
+  it('it should be able to create a like in a post with success', async () => {
     const like = {
-        ownerId: '1',
-        postId: '1',
-    }
+      ownerId: '1',
+      postId: '1',
+    };
     const createdLike = await likeService.create(like);
     expect(createdLike).toEqual(likeMock);
-});
+  });
 
-// it('should not be able to create a like in a post with success', async () => {
-//     prisma.like.create = jest.fn().mockResolvedValue(undefined)
+  it('should not be able to get a like that doest exists', async () => {
+    prisma.like.findUnique = jest.fn().mockResolvedValue(undefined);
 
-//     const like = {
-//         ownerId: '1',
-//         postId: '1',
-//     }
-//     const createdLike = await likeService.create(like);
+    await expect(
+      likeService.findOne('pipipipipi-00000-popopopo-000000'),
+    ).rejects.toThrowError(
+      'Like pipipipipi-00000-popopopo-000000 was not found',
+    );
+  });
 
-//     await expect(likeService.create(like)).rejects.toThrowError('Something wrong happened when you tried to like this post.');
-// });
-
-it('should be able to delete a like from a post', async () => {
+  it('should be able to delete a like from a post', async () => {
     const deletedLike = await likeService.remove(
-        'pipipipipi-00000-popopopo-000000'
+      'pipipipipi-00000-popopopo-000000',
     );
     expect(deletedLike).toEqual(likeMock);
-});
+  });
 });
