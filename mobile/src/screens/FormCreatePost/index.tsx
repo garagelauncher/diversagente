5caf0ab2f8df9672edd1ac3af7a577a07e7f8570
vivianequeiroz <vivianedsqueiroz@gmail.com>
2022-09-04 18:02:23 -0300

    feat(wip): forms boilerplate with react form hook

--- mobile/src/screens/FormCreatePost/index.tsx
+++ mobile/src/screens/FormCreatePost/index.tsx
@@ -1,21 +1,43 @@
-import { Box, Heading, Text } from 'native-base';
-
 import { logger } from '@src/utils/logger';
+import {
+  VStack,
+  Button,
+  FormControl,
+  TextArea,
+} from 'native-base';
+import { useForm, Controller } from 'react-hook-form';
 
 export const FormCreatePost = () => {
   logger.success('FormCreatePost');
-
-  return (
-    <Box
-      width="100%"
-      backgroundColor="yellow.200"
-      flex={1}
-      alignItems="center"
-      justifyContent="center"
-    >
-      <Heading>FormCreatePost</Heading>
-      <Heading>Em breve</Heading>
-      <Text>Para Dezembro de 2022</Text>
-    </Box>
+  const { control, handleSubmit, errors } = useForm();
+  const onSubmit = (data: any) => {
+    console.log('submiting with ', data);
+  };
+    return (
+      <VStack width="80%" space={4}>
+        <FormControl isRequired isInvalid={'thought' in errors}>
+          <FormControl.Label>What do you think?</FormControl.Label>
+          <Controller
+            control={control}
+            render={({ onChange, value }) => (
+              <TextArea
+                placeholder="TextArea"
+                onChangeText={(val) => onChange(val)}
+                defaultValue={value}
+              />
+            )}
+            name="thought"
+            rules={{ required: 'Field is required', minLength: 3 }}
+            defaultValue="I love NativeBase."
+          />
+          <FormControl.ErrorMessage>
+            {errors.thought?.message}
+          </FormControl.ErrorMessage>
+        </FormControl>
+        <Button onPress={handleSubmit(onSubmit)} colorScheme="pink">
+          Submit
+        </Button>
+      </VStack>
+    );
   );
 };
