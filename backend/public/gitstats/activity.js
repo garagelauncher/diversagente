8202513eec8de0d2de725c6f5a6730474d502fca
joaobispo2077 <joaobispo2077@users.noreply.github.com>
2022-10-01 21:06:02 +0000

    docs: update gitstats

--- backend/public/gitstats/activity.js
+++ backend/public/gitstats/activity.js
@@ -1,4 +1,4 @@
-const recent_activity = {"config": {"noData": "No recent activity.", "padData": true, "showXAxis": true, "xDomain": [31, 0]}, "data": [{"key": "Commits", "color": "#9400D3", "values": [{"x": 31, "y": 0}, {"x": 30, "y": 0}, {"x": 29, "y": 0}, {"x": 28, "y": 0}, {"x": 27, "y": 0}, {"x": 26, "y": 0}, {"x": 25, "y": 8}, {"x": 24, "y": 5}, {"x": 23, "y": 0}, {"x": 22, "y": 14}, {"x": 21, "y": 27}, {"x": 20, "y": 4}, {"x": 19, "y": 1}, {"x": 18, "y": 54}, {"x": 17, "y": 91}, {"x": 16, "y": 52}, {"x": 15, "y": 69}, {"x": 14, "y": 0}, {"x": 13, "y": 7}, {"x": 12, "y": 28}, {"x": 11, "y": 0}, {"x": 10, "y": 0}, {"x": 9, "y": 0}, {"x": 8, "y": 0}, {"x": 7, "y": 0}, {"x": 6, "y": 3}, {"x": 5, "y": 0}, {"x": 4, "y": 0}, {"x": 3, "y": 15}, {"x": 2, "y": 65}, {"x": 1, "y": 57}, {"x": 0, "y": 1}]}]}
+const recent_activity = {"config": {"noData": "No recent activity.", "padData": true, "showXAxis": true, "xDomain": [31, 0]}, "data": [{"key": "Commits", "color": "#9400D3", "values": [{"x": 31, "y": 0}, {"x": 30, "y": 0}, {"x": 29, "y": 0}, {"x": 28, "y": 0}, {"x": 27, "y": 8}, {"x": 26, "y": 5}, {"x": 25, "y": 0}, {"x": 24, "y": 14}, {"x": 23, "y": 27}, {"x": 22, "y": 4}, {"x": 21, "y": 1}, {"x": 20, "y": 54}, {"x": 19, "y": 91}, {"x": 18, "y": 52}, {"x": 17, "y": 69}, {"x": 16, "y": 0}, {"x": 15, "y": 7}, {"x": 14, "y": 28}, {"x": 13, "y": 0}, {"x": 12, "y": 0}, {"x": 11, "y": 0}, {"x": 10, "y": 0}, {"x": 9, "y": 0}, {"x": 8, "y": 3}, {"x": 7, "y": 0}, {"x": 6, "y": 0}, {"x": 5, "y": 15}, {"x": 4, "y": 65}, {"x": 3, "y": 57}, {"x": 2, "y": 4}, {"x": 1, "y": 0}, {"x": 0, "y": 12}]}]}
 
 // Setup the chart
 nv.addGraph(function() {
@@ -9,7 +9,7 @@ nv.addGraph(function() {
 	return chart;
 });
 
-const commits_by_year = {"xAxis": {"rotateLabels": -90, "ticks": 1}, "yAxis": {"axisLabel": "Commits"}, "data": [{"key": "Commits", "color": "#9400D3", "values": [{"x": 2022, "y": 501}]}]}
+const commits_by_year = {"xAxis": {"rotateLabels": -90, "ticks": 1}, "yAxis": {"axisLabel": "Commits"}, "data": [{"key": "Commits", "color": "#9400D3", "values": [{"x": 2022, "y": 516}]}]}
 // Setup the chart
 nv.addGraph(function() {
 	var chart = nv.models.historicalBarChart().options({"padData": true, "showXAxis": true});
@@ -19,7 +19,7 @@ nv.addGraph(function() {
 	return chart;
 });
 
-const commits_by_month = {"yAxis": {"axisLabel": "Commits in 2022"}, "xAxis": {"rotateLabels": -90, "ticks": 12}, "data": [{"key": "Commits", "color": "#9400D3", "values": [{"x": 0, "y": 0}, {"x": 1, "y": 0}, {"x": 2, "y": 13}, {"x": 3, "y": 45}, {"x": 4, "y": 267}, {"x": 5, "y": 35}, {"x": 6, "y": 3}, {"x": 7, "y": 22}, {"x": 8, "y": 116}, {"x": 9, "y": 0}, {"x": 10, "y": 0}, {"x": 11, "y": 0}]}]}
+const commits_by_month = {"yAxis": {"axisLabel": "Commits in 2022"}, "xAxis": {"rotateLabels": -90, "ticks": 12}, "data": [{"key": "Commits", "color": "#9400D3", "values": [{"x": 0, "y": 0}, {"x": 1, "y": 0}, {"x": 2, "y": 13}, {"x": 3, "y": 45}, {"x": 4, "y": 267}, {"x": 5, "y": 35}, {"x": 6, "y": 3}, {"x": 7, "y": 22}, {"x": 8, "y": 124}, {"x": 9, "y": 7}, {"x": 10, "y": 0}, {"x": 11, "y": 0}]}]}
 // Setup the chart
 nv.addGraph(function() {
 	var chart = nv.models.historicalBarChart().options({"padData": true, "showXAxis": true});
@@ -49,7 +49,7 @@ nv.addGraph(function() {
       .datum([{
         key: "Cumulative Return",
         color: "#9400D3",
-        values: [{"label": "= 0s", "value": 497}, {"label": "< 1hour", "value": 4}, {"label": "< 1day", "value": 0}, {"label": "< 2days", "value": 0}, {"label": "< 1week", "value": 0}, {"label": "< 2weeks", "value": 0}, {"label": "< 1month", "value": 0}, {"label": "< 6 months", "value": 0}, {"label": "< 3 years", "value": 0}]
+        values: [{"label": "= 0s", "value": 511}, {"label": "< 1hour", "value": 5}, {"label": "< 1day", "value": 0}, {"label": "< 2days", "value": 0}, {"label": "< 1week", "value": 0}, {"label": "< 2weeks", "value": 0}, {"label": "< 1month", "value": 0}, {"label": "< 6 months", "value": 0}, {"label": "< 3 years", "value": 0}]
        }
   ]).call(chart);
 
