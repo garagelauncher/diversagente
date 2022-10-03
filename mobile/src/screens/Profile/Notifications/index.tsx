837343af61c7b797dae54b9975c8fb60f6042b64
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2022-10-03 17:42:20 -0300

    create notifications scream

---
+++ mobile/src/screens/Profile/Notifications/index.tsx
@@ -0,0 +1,59 @@
+import {
+  ScrollView,
+  Button,
+  Text,
+  VStack,
+  Box,
+  Flex,
+  Switch,
+} from 'native-base';
+import React from 'react';
+
+import { theme } from '@src/styles/theme';
+
+export const Notifications = () => {
+  return (
+    <ScrollView>
+      <VStack width={'100%'} height={500} marginTop={180}>
+        <Text
+          fontSize={'2xl'}
+          textAlign={'center'}
+          marginTop={55}
+          bold
+          color={theme.colors.warmGray700}
+        >
+          Notificações
+        </Text>
+        <Box
+          backgroundColor={theme.colors.warmGray200}
+          height={50}
+          marginLeft={30}
+          marginRight={30}
+          marginTop={50}
+          borderRadius={16}
+        >
+          <Flex direction="row">
+            <Text
+              fontSize={'sm'}
+              backgroundColor={theme.colors.warmGray200}
+              bold
+              marginTop={4}
+              marginLeft={2}
+            >
+              Ativar notificações
+            </Text>
+            <Switch size={'sm'} marginTop={3} marginLeft={120}></Switch>
+          </Flex>
+        </Box>
+        <Button
+          marginTop={10}
+          marginLeft={30}
+          marginRight={30}
+          background={theme.colors.blue700}
+        >
+          Salvar escolha
+        </Button>
+      </VStack>
+    </ScrollView>
+  );
+};
