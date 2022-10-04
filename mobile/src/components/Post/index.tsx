4d9188c94852ac010f34db8fb4d4746cb4946e3c
João Bispo <joaobispo2077@gmail.com>
2022-10-04 17:58:29 -0300

    feat: add hasLiked prop with two different hearts

--- mobile/src/components/Post/index.tsx
+++ mobile/src/components/Post/index.tsx
@@ -1,5 +1,14 @@
 import { Feather } from '@expo/vector-icons';
-import { Avatar, Flex, Heading, HStack, Icon, Text } from 'native-base';
+import {
+  Avatar,
+  Flex,
+  Heading,
+  HStack,
+  Icon,
+  Text,
+  FavouriteIcon,
+  Pressable,
+} from 'native-base';
 import { FunctionComponent } from 'react';
 
 import type { Post as PostData } from '@src/contracts/Post';
@@ -9,13 +18,22 @@ import { formatDateSocialMedia } from '@src/utils/time';
 export type PostProps = {
   post: PostData;
   isPreview?: boolean;
+  hasLiked?: boolean;
 };
 
-export const Post: FunctionComponent<PostProps> = ({ post, isPreview }) => {
+export const Post: FunctionComponent<PostProps> = ({
+  post,
+  isPreview,
+  hasLiked = false,
+}) => {
   const userInitials = getUsernameInitials(post.owner.username);
   const contentPreview = post.content.slice(0, 255);
 
   const formattedCreatedAtDate = formatDateSocialMedia(post.createdAt);
+
+  const handleTogglePostLike = () => {
+    console.log('Like toggled');
+  };
 
   return (
     <Flex backgroundColor="white" borderRadius={6} paddingX={6} paddingY={5}>
@@ -43,14 +61,29 @@ export const Post: FunctionComponent<PostProps> = ({ post, isPreview }) => {
 
       <HStack direction="row" space={5} marginTop={3}>
         <Flex direction="row" alignItems="center">
-          <Icon
-            as={Feather}
-            name="heart"
-            size={7}
-            fill="red.500"
-            color="blue"
-            borderRadius={6}
-          />
+          {hasLiked ? (
+            <Pressable
+              onPress={handleTogglePostLike}
+              _pressed={{ backgroundColor: 'gray.100' }}
+              borderRadius={6}
+            >
+              <FavouriteIcon size={7} color="red.500" />
+            </Pressable>
+          ) : (
+            <Pressable
+              onPress={handleTogglePostLike}
+              _pressed={{ backgroundColor: 'gray.100' }}
+              borderRadius={6}
+            >
+              <Icon
+                as={Feather}
+                name="heart"
+                size={7}
+                color="muted.400"
+                borderRadius={6}
+              />
+            </Pressable>
+          )}
           <Text marginLeft={2} fontSize={18}>
             {post._count.likes}
           </Text>
