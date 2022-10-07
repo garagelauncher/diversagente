3708b40ef7b259fe158465224e308367f28843fd
João Bispo <joaobispo2077@gmail.com>
2022-10-07 17:07:50 -0300

    refactor: remove unnecessary statements

--- mobile/src/utils/time.ts
+++ mobile/src/utils/time.ts
@@ -7,20 +7,15 @@ locale('pt-br');
 extend(relativeTime);
 
 export const formatDateSocialMedia = (date: string, locale = 'pt-br') => {
-  const isPostedInLast24Hours = dayjs().locale(locale).diff(date, 'day') < 1;
-  const isPostedInLastHour = dayjs().locale(locale).diff(date, 'hour') < 1;
+  const isPostedToday = dayjs(date).isSame(dayjs(), 'day');
 
-  if (isPostedInLastHour) {
+  if (isPostedToday) {
     return dayjs(date).locale(locale).fromNow();
   }
 
-  if (isPostedInLast24Hours) {
-    return dayjs(date).locale(locale).format('HH:mm');
-  }
-
   if (locale === 'pt-br') {
-    return dayjs(date).locale(locale).format('DD/MM/YYYY');
+    return dayjs(date).locale(locale).format('DD/MM/YYYY HH:mm');
   }
 
-  return dayjs(date).locale(locale).format('MM/DD/YYYY');
+  return dayjs(date).locale(locale).format('MM/DD/YYYY HH:mm');
 };
