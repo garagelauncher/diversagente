5521091db6a793f6450446b2104618302df41128
vivianequeiroz <vivianedsqueiroz@gmail.com>
2022-10-08 13:41:47 -0300

    feat: add function to retrieve subcategories related to a specific category

--- mobile/src/services/diversaGente/subcategories/index.ts
+++ mobile/src/services/diversaGente/subcategories/index.ts
@@ -2,6 +2,12 @@ import { diversagenteBaseApi } from '../baseUrl';
 
 import { Subcategory } from '@src/contracts/Subcategory';
 
+export type filterSubcategory = {
+  categoriesIds: {
+    hasSome: [categoryId: string];
+  };
+};
+
 export const findAllSubcategories = async () => {
   try {
     const response = await diversagenteBaseApi.get<Subcategory[]>(
@@ -10,6 +16,26 @@ export const findAllSubcategories = async () => {
     const categories = response.data;
     console.info('SUBCATEGORIES!', response.data);
     return categories;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+export const findRelatedSubcategoriesToCategory = async (
+  categoryId: string,
+  filterParams: filterSubcategory,
+) => {
+  console.log('categoryId in service', categoryId);
+  console.log('filter in service', filterParams);
+  try {
+    const response = await diversagenteBaseApi.get<Subcategory[]>(
+      `/subcategories`,
+      { params: { filter: filterParams } },
+    );
+    const relatedSubcategoriesToCategory = response.data;
+    console.info('SUBCATEGORIES FILTERED BY CATEGORY', response.data);
+    return relatedSubcategoriesToCategory;
   } catch (error) {
     console.error(error);
     throw error;
