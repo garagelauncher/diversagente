80413d516f280dc168712c31034643ba1138b22e
vivianequeiroz <vivianedsqueiroz@gmail.com>
2022-10-09 20:35:37 -0300

    feat: add object type to queryoptions

--- mobile/src/contracts/QueryOptions.ts
+++ mobile/src/contracts/QueryOptions.ts
@@ -1,6 +1,6 @@
 export type QueryOptions = {
   range?: [number, number];
-  sort?: [string, 'ASC' | 'DESC'];
+  sort?: [string, 'ASC' | 'DESC' | object];
   filter?: Record<string, any>;
   include?: Record<string, any>;
   cursor?: Record<string, any>;
