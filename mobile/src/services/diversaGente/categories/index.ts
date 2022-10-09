96ab72434187dbcdb14d3f0df5d315b70855b21e
João Bispo <joaobispo2077@gmail.com>
2022-10-09 16:34:31 -0300

    feat: exchange of useQuery by useInfinityQuery from useCategories

--- mobile/src/services/diversaGente/categories/index.ts
+++ mobile/src/services/diversaGente/categories/index.ts
@@ -1,13 +1,17 @@
 import { diversagenteBaseApi } from '../baseUrl';
 
 import { Category } from '@src/contracts/Category';
+import { QueryOptions } from '@src/contracts/QueryOptions';
+import { parseQueryOptions } from '@src/utils/parseQuery';
 
-export const findAllCategories = async () => {
+export const findAllCategories = async (options: QueryOptions = {}) => {
   try {
-    const response = await diversagenteBaseApi.get<Category[]>('/categories');
+    const response = await diversagenteBaseApi.get<Category[]>('/categories', {
+      params: parseQueryOptions(options),
+    });
     const categories = response.data;
     // console.debug('CATEGORIES', categories);
-    return categories;
+    return { results: categories };
   } catch (error: any) {
     console.error('error when fetchin all categories');
 
