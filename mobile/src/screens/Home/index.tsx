b978869a4f60228e85683c2c6113fcc043b76d16
João Bispo <joaobispo2077@gmail.com>
2022-10-11 19:14:01 -0300

    refactor: remove unused code

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,6 +1,5 @@
 import { Feather } from '@expo/vector-icons';
 import { NavigationProp, useNavigation } from '@react-navigation/native';
-import * as Linking from 'expo-linking';
 import {
   FlatList,
   Flex,
@@ -32,33 +31,7 @@ export type HomeScreenNavigationProps = NavigationProp<
   'Home'
 >;
 
-const useMount = (func: CallableFunction) => useEffect(() => func(), [func]);
-
-const useInitialURL = () => {
-  const [url, setUrl] = useState<string | null>(null);
-  const [processing, setProcessing] = useState(true);
-
-  useMount(() => {
-    const getUrlAsync = async () => {
-      // Get the deep link used to open the app
-      const initialUrl = await Linking.getInitialURL();
-
-      // The setTimeout is just for testing purpose
-      setTimeout(() => {
-        setUrl(initialUrl);
-        setProcessing(false);
-      }, 1000);
-    };
-
-    getUrlAsync();
-  });
-
-  return { url, processing };
-};
-
 export const Home = () => {
-  // const { url: initialUrl, processing } = useInitialURL();
-
   const toast = useToast();
   const { user } = useAuth();
   const navigation = useNavigation<HomeScreenNavigationProps>();
@@ -176,11 +149,6 @@ export const Home = () => {
           alignItems="center"
           justifyContent="space-between"
         >
-          {/* <Text>
-            {processing
-              ? `Processing the initial url from a deep link`
-              : `The deep link is: ${initialUrl || 'None'}`}
-          </Text> */}
           <Heading>Últimas postagens</Heading>
           <IconButton
             colorScheme={isReadingModeActive ? 'orange' : 'coolGray'}
