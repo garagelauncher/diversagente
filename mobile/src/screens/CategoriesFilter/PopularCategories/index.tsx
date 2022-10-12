d71a8e1115a6f68bb0a4816a61bca5a48debee9b
vivianequeiroz <vivianedsqueiroz@gmail.com>
2022-10-12 17:41:11 -0300

    refact: rename categories list screen to category filter

---
+++ mobile/src/screens/CategoriesFilter/PopularCategories/index.tsx
@@ -0,0 +1,130 @@
+import {
+  Box,
+  Flex,
+  Text,
+  VStack,
+  HStack,
+  Alert,
+  Link,
+  FlatList,
+  Skeleton,
+  Spinner,
+} from 'native-base';
+import React from 'react';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+
+import { useCategories } from '@src/hooks/queries/useCategories';
+import { calculatePreviousDateAccordingToRange } from '@src/utils/calculatePreviousDateAccordingToRange';
+
+export const PopularCategories = () => {
+  const lastWeekDate = calculatePreviousDateAccordingToRange(7).toISOString();
+  console.log(lastWeekDate, 'lastWeekDate');
+
+  const { data, hasNextPage, fetchNextPage, isLoading, isFetchingNextPage } =
+    useCategories({
+      sort: ['Post', { _count: 'desc' }],
+      //  filter: {
+      //    Post: { every: { createdAt: { gte: '2022-10-09T22:21:15.503Z' } } },
+      //   },
+    });
+
+  const handleLoadMorePopularCategories = () => {
+    console.log('load more popular categories');
+    if (hasNextPage) {
+      fetchNextPage();
+    }
+  };
+
+  const skeletonsCategories = new Array(5).fill(0);
+  return (
+    <>
+      {(isLoading || isFetchingNextPage) &&
+        skeletonsCategories.map((_, index) => (
+          <Skeleton
+            key={index}
+            height={73}
+            width={'90%'}
+            borderRadius={8}
+            alignItems={'center'}
+            marginBottom={4}
+          ></Skeleton>
+        ))}
+      <FlatList
+        width={'100%'}
+        data={data?.pages.map((page) => page.results).flat()}
+        renderItem={({ item }) => (
+          <TouchableOpacity>
+            <Box marginBottom={4}>
+              <Flex
+                h={73}
+                w={'100%'}
+                bg={'blue.50'}
+                borderColor={'darkBlue.600'}
+                borderWidth={1.5}
+                opacity={0.8}
+                rounded="md"
+                justifyContent="center"
+              >
+                <Text fontSize="xl" justifyContent="center" py={5} px={5} bold>
+                  {item.title}
+                </Text>
+              </Flex>
+            </Box>
+          </TouchableOpacity>
+        )}
+        keyExtractor={(item) => item.id}
+        contentContainerStyle={{ padding: 20 }}
+        onEndReached={handleLoadMorePopularCategories}
+        onEndReachedThreshold={0.9}
+        ListFooterComponent={
+          isFetchingNextPage && !hasNextPage ? (
+            <Spinner color="orange.500" size="sm" />
+          ) : (
+            <Alert
+              key="alertInfo"
+              status="info"
+              colorScheme="info"
+              paddingX={2}
+            >
+              <VStack space={2} w="90%">
+                <HStack
+                  flexShrink={1}
+                  space={2}
+                  alignItems="center"
+                  justifyContent="space-between"
+                >
+                  <HStack flexShrink={1} space={2} alignItems="center">
+                    <Alert.Icon />
+                    <Text
+                      fontSize="md"
+                      fontWeight="medium"
+                      color="coolGray.800"
+                    >
+                      Sentindo falta de alguma categoria?
+                    </Text>
+                  </HStack>
+                </HStack>
+                <Box
+                  pl="6"
+                  _text={{
+                    color: 'coolGray.600',
+                  }}
+                >
+                  <Link
+                    _text={{
+                      color: 'info.700',
+                      fontWeight: 'bold',
+                      fontSize: 'sm',
+                    }}
+                  >
+                    Nos envie um e-mail com a sua sugestão!
+                  </Link>
+                </Box>
+              </VStack>
+            </Alert>
+          )
+        }
+      />
+    </>
+  );
+};
