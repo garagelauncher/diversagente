b149827de41b3ecf9e1da723fcb5623856225b1c
vivianequeiroz <vivianedsqueiroz@gmail.com>
2022-10-15 18:05:36 -0300

    feat: add navigation to category filter screen from a popular or favorite category

--- mobile/src/screens/SubcategoriesFilter/index.tsx
+++ mobile/src/screens/SubcategoriesFilter/index.tsx
@@ -11,8 +11,6 @@ import {
 import React from 'react';
 
 import { HeaderSubcategories } from '@src/components/HeaderSubcategories';
-import { Subcategory } from '@src/components/Subcategory';
-import { theme } from '@src/styles/theme';
 
 export const SubcategoryFilter = () => {
   return (
@@ -23,12 +21,8 @@ export const SubcategoryFilter = () => {
           'Aqui você pode visualizar todas as subcategorias que\nse relacionam com a categoria Saúde.\nVocê pode criar novas subcategorias para esse tema \ne dentro delas criar, curtir e comentar post.'
         }
       ></HeaderSubcategories>
-      <ScrollView backgroundColor={theme.colors.light50} height={'100%'}>
-        <HStack
-          backgroundColor={theme.colors.light50}
-          width={'100%'}
-          height={70}
-        >
+      <ScrollView height={'100%'}>
+        <HStack width={'100%'} height={70}>
           <Box marginTop={8} width={'100%'} marginLeft={4}>
             <HStack space={4} alignItems="center" width={'100%'}>
               <Flex direction="row" width={'100%'}>
@@ -37,14 +31,12 @@ export const SubcategoryFilter = () => {
                   placeholder="Pesquise uma subcategoria"
                   borderRadius={90}
                   width={'65%'}
-                  borderColor={theme.colors.warmGray700}
                   InputRightElement={
                     <Icon
                       as={AntDesign}
                       name="search1"
                       size={4}
                       marginRight={3}
-                      color={theme.colors.warmGray700}
                     />
                   }
                 />
@@ -54,22 +46,14 @@ export const SubcategoryFilter = () => {
                   width={35}
                   height={35}
                   marginLeft={2}
-                  backgroundColor={theme.colors.primaryColor}
                 >
-                  <Icon
-                    as={AntDesign}
-                    name="plussquareo"
-                    size={8}
-                    color={theme.colors.warmGray700}
-                  />
+                  <Icon as={AntDesign} name="plussquareo" size={8} />
                 </Button>
               </Flex>
             </HStack>
           </Box>
         </HStack>
-        <Box marginTop={4} width={'100%'} marginLeft={4}>
-          <Subcategory></Subcategory>
-        </Box>
+        <Box marginTop={4} width={'100%'} marginLeft={4}></Box>
       </ScrollView>
     </>
   );
