65e819a97a28903039c126e394cd89bf4fac99e2
João Bispo <joaobispo2077@gmail.com>
2022-10-16 17:55:03 -0300

    refactor: adjust time returns

--- mobile/src/utils/time.ts
+++ mobile/src/utils/time.ts
@@ -6,16 +6,20 @@ import relativeTime from 'dayjs/plugin/relativeTime';
 locale('pt-br');
 extend(relativeTime);
 
-export const formatDateSocialMedia = (date: string, locale = 'pt-br') => {
+export const formatDateSocialMedia = (
+  date: string,
+  locale = 'pt-br',
+): string => {
   const isPostedToday = dayjs(date).isSame(dayjs(), 'day');
 
   if (isPostedToday) {
     return dayjs(date).locale(locale).fromNow();
   }
 
-  if (locale === 'pt-br') {
-    return dayjs(date).locale(locale).format('DD/MM/YYYY HH:mm');
-  }
-
-  return dayjs(date).locale(locale).format('MM/DD/YYYY HH:mm');
+  return new Intl.DateTimeFormat(locale, {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+    weekday: 'long',
+  }).format(new Date(date));
 };
