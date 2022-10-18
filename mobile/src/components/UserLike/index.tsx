07182a4165b09dd07856be195c0d7dae32a249c2
João Bispo <joaobispo2077@gmail.com>
2022-10-18 17:55:48 -0300

    refactor: adjust user like style

--- mobile/src/components/UserLike/index.tsx
+++ mobile/src/components/UserLike/index.tsx
@@ -2,15 +2,15 @@ import { Avatar, Flex, Text } from 'native-base';
 import { FunctionComponent } from 'react';
 
 import { IncludeInto } from '@src/@types/generics/includeInto';
-import { getUsernameInitials } from '@src/utils/getUsernameInitials';
 import { Like, LikeOwner } from '@src/contracts/Like';
+import { getUsernameInitials } from '@src/utils/getUsernameInitials';
 
 export type UserLikeProps = {
   like: IncludeInto<Like, { owner: LikeOwner }>;
 };
 
 export const UserLike: FunctionComponent<UserLikeProps> = ({
-    like: { owner },
+  like: { owner },
 }) => {
   const userInitials = getUsernameInitials(owner.name);
 
@@ -18,20 +18,25 @@ export const UserLike: FunctionComponent<UserLikeProps> = ({
     <Flex
       backgroundColor="white"
       borderRadius={6}
-      paddingX={6}
-      paddingY={5}
+      paddingX={3}
+      paddingY={4}
       width="100%"
+      direction="row"
+      alignItems="center"
+      justifyContent="flex-start"
     >
-        <Avatar
-          borderRadius={6}
-          backgroundColor={'primary.500'}
-          source={{
-            uri: String(owner.picture),
-          }}
-        >
-          {userInitials}
-        </Avatar>
-        <Text fontWeight={'bold'} marginLeft={5}>{owner.name}</Text>
+      <Avatar
+        borderRadius={6}
+        backgroundColor={owner.picture ? 'transparent' : 'primary.500'}
+        source={{
+          uri: String(owner.picture),
+        }}
+      >
+        {userInitials}
+      </Avatar>
+      <Text fontWeight={'bold'} marginLeft={5}>
+        {owner.name}
+      </Text>
     </Flex>
   );
 };
