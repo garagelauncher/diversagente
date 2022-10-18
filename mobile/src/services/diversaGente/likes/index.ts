9beeb3eca9d34802977c9ca45a834184bcbb8794
joaobispo2077 <joaobispo2077@gmail.com>
2022-10-17 21:47:56 -0300

    feat: add useLikes hook with fetch likes service

--- mobile/src/services/diversaGente/likes/index.ts
+++ mobile/src/services/diversaGente/likes/index.ts
@@ -1,12 +1,44 @@
 import { diversagenteBaseApi } from '../baseUrl';
 
+import { IncludeInto } from '@src/@types/generics/includeInto';
+import { parseQueryOptions } from '@src/utils/parseQuery';
+import { QueryOptions } from '@src/contracts/QueryOptions';
+import { Like } from '@src/contracts/Like';
+
 export type CreateLikeDTO = {
   postId: string;
   ownerId: string;
 };
+
 export type DeleteLikeDTO = {
   postId: string;
   likeId: string;
+};
+
+export const findAllLikes = async <GenericIncluded extends object = object>(
+  postId: string,
+  options: QueryOptions = {},
+) => {
+  try {
+    const response = await diversagenteBaseApi.get<
+      IncludeInto<Like, GenericIncluded>[]
+    >(`/posts/${postId}/likes/`, {
+      params: parseQueryOptions(options),
+    });
+    const likes = response.data;
+
+    return { results: likes };
+  } catch (error: any) {
+    console.error('error when fetch all likes');
+    console.error(postId);
+    console.error(options);
+
+    if (error.isAxiosError) {
+      console.error(error.response);
+    }
+
+    throw error;
+  }
 };
 
 export const createLike = async ({ postId, ownerId }: CreateLikeDTO) => {
