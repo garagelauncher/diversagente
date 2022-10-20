251f8676317ae70146f29f22b471c7197cdd6c3d
vivianequeiroz <vivianedsqueiroz@gmail.com>
2022-10-19 22:01:12 -0300

    feat: add post loading using usePosts hooks filtering by subcategory id

--- mobile/src/screens/Subcategory/index.tsx
+++ mobile/src/screens/Subcategory/index.tsx
@@ -18,15 +18,14 @@ import {
   Input,
 } from 'native-base';
 import React from 'react';
-import { TouchableOpacity } from 'react-native';
-import { useQuery } from 'react-query';
 
 import { Header } from '@src/components/Header';
 import { useSubcategoryDetails } from '@src/hooks/queries/details/useSubcategoryDetails';
-import { useSubcategories } from '@src/hooks/queries/useSubcategories';
 import { useAuth } from '@src/hooks/useAuth';
-import { StackForumNavigatorParamList } from '@src/routes/stacks/forumStack.routes';
-import { diversaGenteServices } from '@src/services/diversaGente';
+import { Post, UserHasInteracted } from '@src/components/Post';
+import { PER_PAGE_ITEMS, userIdHelper } from '@src/configs';
+import { usePosts } from '@src/hooks/queries/usePosts';
+import { LoadingFallback } from '@src/components/LoadingFallback';
 
 type SubcategoriesNavigationProps = NavigationProp<
   StackForumNavigatorParamList,
@@ -39,9 +38,54 @@ export const Subcategory = () => {
   const { subcategoryId } = route.params;
   const { user } = useAuth();
 
-  const { data, isLoading, isError } = useSubcategoryDetails(subcategoryId);
+  const { result, isLoading, isError } = useSubcategoryDetails(subcategoryId);
 
-  console.log(data);
+  const postFilters = {
+    subcategoryId: subcategoryId,
+  };
+
+
+  const {
+    data,
+    hasNextPage,
+    fetchNextPage,
+    isLoading,
+    isFetchingNextPage,
+    refetch,
+    isRefetching,
+  } = usePosts<UserHasInteracted>({
+    range: [0, PER_PAGE_ITEMS],
+    sort: ['createdAt', 'DESC'],
+    filter: {
+      ...(postFilters.subcategoryId ? postFilters : {}),
+    },
+    include: {
+      likes: {
+        select: { id: true },
+        where: { ownerId: user?.id ?? userIdHelper },
+      },
+      comments: {
+        select: { id: true },
+        where: { ownerId: user?.id ?? userIdHelper },
+      },
+    },
+  });
+
+  const handleLoadMorePosts = () => {
+    if (hasNextPage) {
+      fetchNextPage();
+    }
+  };
+
+  const handlePullPostListToRefresh = () => {
+    refetch();
+  };
+
+  const handleNavigateToFormCreatePost = () => {
+    navigation.navigate('FormCreatePost');
+  };
+
+
 
   const skeletonsPosts = new Array(2).fill(0);
 
@@ -53,11 +97,56 @@ export const Subcategory = () => {
         <Box marginBottom={4}>
           <Header
             avatar={user?.picture}
-            screenName={`${data?.title}`}
-            subtitle={`${data?.description}`}
+            screenName={`${result?.title}`}
+            subtitle={`${result?.description}`}
           ></Header>
         </Box>
       </Box>
+
+      <LoadingFallback
+          isLoading={isLoading}
+          fallback={
+            <VStack space={4}>
+              <Skeleton width="100%" height={200} />
+              <Skeleton width="100%" height={200} />
+              <Skeleton width="100%" height={200} />
+            </VStack>
+          }
+        >
+          <FlatList
+            width={'100%'}
+            data={data?.pages.map((page) => page.results).flat()}
+            renderItem={({ item }) => (
+              <Box marginBottom={4}>
+                <Post post={item} isPreview />
+              </Box>
+            )}
+            keyExtractor={(item) => item.id}
+            contentContainerStyle={{ paddingBottom: 350 }}
+            onEndReached={handleLoadMorePosts}
+            onEndReachedThreshold={0.85}
+            refreshing={isRefetching && !isFetchingNextPage}
+            onRefresh={handlePullPostListToRefresh}
+            ListFooterComponent={
+              <LoadingFallback
+                fallback={<Spinner color="orange.500" size="md" />}
+                isLoading={isFetchingNextPage}
+              >
+                <Flex width="100%" alignItems="center" justifyContent="center">
+                  <Text color="gray.500">
+                    Não há mais postagens nessa subcategoria.
+                  </Text>
+                  <Text
+                    color="blue.500"
+                    onPress={handleNavigateToFormCreatePost}
+                  >
+                    Você pode clicar aqui para criar um novo post!
+                  </Text>
+                </Flex>
+              </LoadingFallback>
+            }
+          />
+        </LoadingFallback>
     </>
   );
 };
