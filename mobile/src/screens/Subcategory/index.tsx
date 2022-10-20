d109c5853a0fa4cb11951be393f53b292a704662
vivianequeiroz <vivianedsqueiroz@gmail.com>
2022-10-20 17:54:14 -0300

    feat: add post listing at a subcategory

--- mobile/src/screens/Subcategory/index.tsx
+++ mobile/src/screens/Subcategory/index.tsx
@@ -1,4 +1,4 @@
-import { AntDesign } from '@expo/vector-icons';
+import { AntDesign, Feather } from '@expo/vector-icons';
 import {
   NavigationProp,
   RouteProp,
@@ -16,34 +16,45 @@ import {
   HStack,
   Icon,
   Input,
+  VStack,
+  Heading,
+  IconButton,
 } from 'native-base';
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+import { TouchableOpacity } from 'react-native';
+
+import { SubcategoryHeader } from './SubcategoryHeader';
 
 import { Header } from '@src/components/Header';
-import { useSubcategoryDetails } from '@src/hooks/queries/details/useSubcategoryDetails';
-import { useAuth } from '@src/hooks/useAuth';
+import { LoadingFallback } from '@src/components/LoadingFallback';
 import { Post, UserHasInteracted } from '@src/components/Post';
 import { PER_PAGE_ITEMS, userIdHelper } from '@src/configs';
+import { useSubcategoryDetails } from '@src/hooks/queries/details/useSubcategoryDetails';
 import { usePosts } from '@src/hooks/queries/usePosts';
-import { LoadingFallback } from '@src/components/LoadingFallback';
+import { useAuth } from '@src/hooks/useAuth';
+import { StackForumNavigatorParamList } from '@src/routes/stacks/forumStack.routes';
 
 type SubcategoriesNavigationProps = NavigationProp<
   StackForumNavigatorParamList,
-  'Subcategory'
+  'Subcategory',
+  'FormCreatePost'
 >;
 
 export const Subcategory = () => {
   const route =
     useRoute<RouteProp<StackForumNavigatorParamList, 'Subcategory'>>();
-  const { subcategoryId } = route.params;
+  const { subcategoryId, categoryId } = route.params;
   const { user } = useAuth();
 
-  const { result, isLoading, isError } = useSubcategoryDetails(subcategoryId);
+  const toggleReadingMode = useCallback(() => {
+    setIsReadingModeActive(
+      (previousIsReadingModeActive) => !previousIsReadingModeActive,
+    );
+  }, []);
 
   const postFilters = {
     subcategoryId: subcategoryId,
   };
-
 
   const {
     data,
@@ -82,10 +93,13 @@ export const Subcategory = () => {
   };
 
   const handleNavigateToFormCreatePost = () => {
-    navigation.navigate('FormCreatePost');
+    navigation.navigate('FormCreatePost', {
+      subcategoryId,
+      categoryId,
+    });
   };
 
-
+  const [isReadingModeActive, setIsReadingModeActive] = useState(false);
 
   const skeletonsPosts = new Array(2).fill(0);
 
@@ -93,60 +107,106 @@ export const Subcategory = () => {
 
   return (
     <>
-      <Box bgColor={'darkBlue.700'} height={390} mb={10}>
-        <Box marginBottom={4}>
-          <Header
-            avatar={user?.picture}
-            screenName={`${result?.title}`}
-            subtitle={`${result?.description}`}
-          ></Header>
-        </Box>
-      </Box>
-
-      <LoadingFallback
-          isLoading={isLoading}
-          fallback={
-            <VStack space={4}>
-              <Skeleton width="100%" height={200} />
-              <Skeleton width="100%" height={200} />
-              <Skeleton width="100%" height={200} />
-            </VStack>
-          }
+      <Flex flex={1}>
+        <Flex
+          backgroundColor="gray.100"
+          width="100%"
+          borderTopLeftRadius={16}
+          borderTopRightRadius={16}
+          paddingBottom={40}
+          marginTop={[isReadingModeActive ? 6 : 0]}
         >
-          <FlatList
-            width={'100%'}
-            data={data?.pages.map((page) => page.results).flat()}
-            renderItem={({ item }) => (
-              <Box marginBottom={4}>
-                <Post post={item} isPreview />
-              </Box>
-            )}
-            keyExtractor={(item) => item.id}
-            contentContainerStyle={{ paddingBottom: 350 }}
-            onEndReached={handleLoadMorePosts}
-            onEndReachedThreshold={0.85}
-            refreshing={isRefetching && !isFetchingNextPage}
-            onRefresh={handlePullPostListToRefresh}
-            ListFooterComponent={
-              <LoadingFallback
-                fallback={<Spinner color="orange.500" size="md" />}
-                isLoading={isFetchingNextPage}
+          {!isReadingModeActive && (
+            <>
+              <SubcategoryHeader />
+            </>
+          )}
+
+          <Flex
+            width="100%"
+            padding={6}
+            direction="row"
+            alignItems="center"
+            justifyContent="space-between"
+          >
+            <Heading>Postagens</Heading>
+            <Flex direction="row" alignItems="center">
+              <TouchableOpacity
+                activeOpacity={0.5}
+                onPress={handleNavigateToFormCreatePost}
               >
-                <Flex width="100%" alignItems="center" justifyContent="center">
-                  <Text color="gray.500">
-                    Não há mais postagens nessa subcategoria.
-                  </Text>
-                  <Text
-                    color="blue.500"
-                    onPress={handleNavigateToFormCreatePost}
-                  >
-                    Você pode clicar aqui para criar um novo post!
-                  </Text>
-                </Flex>
-              </LoadingFallback>
+                <Icon
+                  mr={4}
+                  as={AntDesign}
+                  name="plussquareo"
+                  size={'xl'}
+                  color={'darkBlue.800'}
+                />
+              </TouchableOpacity>
+
+              <IconButton
+                colorScheme={isReadingModeActive ? 'orange' : 'coolGray'}
+                variant={'ghost'}
+                size={'lg'}
+                _icon={{
+                  as: Feather,
+                  name: isReadingModeActive ? 'list' : 'grid',
+                }}
+                onPress={toggleReadingMode}
+              />
+            </Flex>
+          </Flex>
+
+          <LoadingFallback
+            isLoading={isLoading}
+            fallback={
+              <VStack space={4}>
+                <Skeleton width="100%" height={200} />
+                <Skeleton width="100%" height={200} />
+                <Skeleton width="100%" height={200} />
+              </VStack>
             }
-          />
-        </LoadingFallback>
+          >
+            <FlatList
+              width={'100%'}
+              data={data?.pages.map((page) => page.results).flat()}
+              renderItem={({ item }) => (
+                <Box marginBottom={4}>
+                  <Post post={item} isPreview />
+                </Box>
+              )}
+              keyExtractor={(item) => item.id}
+              contentContainerStyle={{ paddingBottom: 350 }}
+              onEndReached={handleLoadMorePosts}
+              onEndReachedThreshold={0.85}
+              refreshing={isRefetching && !isFetchingNextPage}
+              onRefresh={handlePullPostListToRefresh}
+              ListFooterComponent={
+                <LoadingFallback
+                  fallback={<Spinner color="orange.500" size="lg" />}
+                  isLoading={isFetchingNextPage}
+                >
+                  <Flex
+                    width="100%"
+                    alignItems="center"
+                    justifyContent="center"
+                  >
+                    <Text color="gray.500">
+                      Não há mais postagens nessa categoria.
+                    </Text>
+                    <Text
+                      color="blue.500"
+                      onPress={handleNavigateToFormCreatePost}
+                    >
+                      Você pode clicar aqui para criar um novo post!
+                    </Text>
+                  </Flex>
+                </LoadingFallback>
+              }
+            />
+          </LoadingFallback>
+        </Flex>
+      </Flex>
     </>
   );
 };
