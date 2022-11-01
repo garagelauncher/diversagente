898d5b093c21b9cfab7ca629840bf5a2d9d0431b
João Bispo <joaobispo2077@gmail.com>
2022-11-01 18:14:58 -0300

    style: lint reviews service

--- mobile/src/services/diversaGente/reviews/index.ts
+++ mobile/src/services/diversaGente/reviews/index.ts
@@ -1,54 +1,55 @@
-import { IncludeInto } from "@src/@types/generics/includeInto";
-import { QueryOptions } from "@src/contracts/QueryOptions";
-import { Review } from "@src/contracts/Review";
-import { parseQueryOptions } from "@src/utils/parseQuery";
-import { diversagenteBaseApi } from "../baseUrl";
+import { diversagenteBaseApi } from '../baseUrl';
 
-export const getReviewsByLocationId = async   <GenericIncluded extends object = object>(
-    locationId: string,
-    options: QueryOptions = {},
-  ) => {
-    try {
-      const response = await diversagenteBaseApi.get<IncludeInto<Review, GenericIncluded>[]>(
-        `/locations/${locationId}/reviews`,
-        {
-            params: parseQueryOptions(options),
-        },
-      );
-  
-      const reviews = response.data;
-      console.info('reviews from a location');
-      console.info(reviews);
-      return reviews;
-    } catch (error: any) {
-      console.info('error when fetching reviews from a location', error);
-      if (error.isAxiosError) {
-        console.error(error.response);
-      }
-      throw error;
+import { IncludeInto } from '@src/@types/generics/includeInto';
+import { QueryOptions } from '@src/contracts/QueryOptions';
+import { Review } from '@src/contracts/Review';
+import { parseQueryOptions } from '@src/utils/parseQuery';
+
+export const getReviewsByLocationId = async <
+  GenericIncluded extends object = object,
+>(
+  locationId: string,
+  options: QueryOptions = {},
+) => {
+  try {
+    const response = await diversagenteBaseApi.get<
+      IncludeInto<Review, GenericIncluded>[]
+    >(`/locations/${locationId}/reviews`, {
+      params: parseQueryOptions(options),
+    });
+
+    const reviews = response.data;
+    console.info('reviews from a location');
+    console.info(reviews);
+    return { results: reviews };
+  } catch (error: any) {
+    console.info('error when fetching reviews from a location', error);
+    if (error.isAxiosError) {
+      console.error(error.response);
     }
+    throw error;
+  }
 };
 
 export const createReviewToLocation = async (
-    locationId: string,
-    review: Partial<Review>,
-  ) => {
-    try {
-      const response = await diversagenteBaseApi.post<Review>(
-        `/locations/${locationId}/reviews`,
-        review,
-      );
-  
-      const createdReview = response.data;
-      console.info('created review');
-      console.info(createdReview);
-      return createdReview;
-    } catch (error: any) {
-      console.info('error when creating review');
-      if (error.isAxiosError) {
-        console.error(error.response);
-      }
-      throw error;
+  locationId: string,
+  review: Partial<Review>,
+) => {
+  try {
+    const response = await diversagenteBaseApi.post<Review>(
+      `/locations/${locationId}/reviews`,
+      review,
+    );
+
+    const createdReview = response.data;
+    console.info('created review');
+    console.info(createdReview);
+    return createdReview;
+  } catch (error: any) {
+    console.info('error when creating review');
+    if (error.isAxiosError) {
+      console.error(error.response);
     }
-  };
-  
+    throw error;
+  }
+};
