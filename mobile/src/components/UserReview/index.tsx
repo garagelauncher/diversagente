85d7995afde65b859efdaef2f7f194388869b207
João Bispo <joaobispo2077@gmail.com>
2022-11-02 17:17:46 -0300

    refactor: restyle review component

--- mobile/src/components/UserReview/index.tsx
+++ mobile/src/components/UserReview/index.tsx
@@ -1,73 +1,74 @@
 import { Fontisto } from '@expo/vector-icons';
-import { Avatar, Box, Divider, HStack, Text, VStack } from 'native-base';
-import { FunctionComponent } from 'react';
+import { Avatar, Divider, Flex, Text } from 'native-base';
+import { FunctionComponent, useState } from 'react';
 import { AirbnbRating } from 'react-native-ratings';
 
 import { Review } from '@src/contracts/Review';
-import { formatDate } from '@src/utils/formatDate';
+import { useAuth } from '@src/hooks/useAuth';
+import { getUsernameInitials } from '@src/utils/getUsernameInitials';
+import { formatDateSocialMedia } from '@src/utils/time';
 
 export type UserReviewProps = {
   review: Review;
 };
 
 export const UserReview: FunctionComponent<UserReviewProps> = ({ review }) => {
+  const { user } = useAuth();
+  const [isEditModeActive, setIsEditModeActive] = useState(false);
+
+  const isOwner = user?.id === review.owner.id;
+  const userInitials = getUsernameInitials(review.owner.name);
+  const formattedCreatedAtDate = formatDateSocialMedia(review.createdAt);
+
   return (
-    <Box
-      size="md"
-      borderRadius={20}
-      borderColor={'warning.300'}
-      borderWidth={1}
-      _text={{
-        color: 'amber.50',
-      }}
-      w={'100%'}
-      h={'auto'}
-      mt={10}
-      px={4}
-      py={8}
-      bg={'warning.100'}
+    <Flex
+      backgroundColor="white"
+      borderRadius={6}
+      paddingX={6}
+      paddingY={5}
+      width="100%"
     >
-      <Fontisto name="quote-a-right" size={28} color="black" />
-      <Text mb={5} mt={4} fontSize={20}>
+      <Flex
+        direction="row"
+        alignItems="flex-start"
+        justifyContent="space-between"
+      >
+        <Flex direction="row" alignItems="center">
+          <Avatar
+            borderRadius={6}
+            backgroundColor={
+              review.owner.picture ? 'transparent' : 'primary.500'
+            }
+            source={{
+              uri: String(review.owner.picture),
+            }}
+          >
+            {userInitials}
+          </Avatar>
+          <Flex marginLeft={5}>
+            <Text fontWeight={'bold'}>{review.owner.name}</Text>
+            <Text color="gray.500">{formattedCreatedAtDate}</Text>
+          </Flex>
+        </Flex>
+        <Flex>
+          {/* <CommentMoreActions
+          isOwner={isOwner}
+          onActivateCommentEditMode={() => setIsEditModeActive(true)}
+          commentId={id}
+        /> */}
+        </Flex>
+      </Flex>
+      <Text mt={4} fontSize={20}>
         {review.text}
       </Text>
-      <Divider my={5} bg={'muted.800'} />
-      <HStack bottom={0} alignItems="center" justifyContent="space-evenly">
-        <HStack alignItems="center">
-          {review.owner.picture ? (
-            <Avatar
-              size="md"
-              source={{
-                uri: review.owner.picture,
-              }}
-            >
-              {review.owner.name}
-            </Avatar>
-          ) : (
-            <Avatar size="md" bg={'info.600'}>
-              {review.owner.name[0]}
-            </Avatar>
-          )}
-
-          <VStack pl={2} pr={5}>
-            <Text fontSize={16} color={'muted.800'}>
-              {review.owner.name}
-            </Text>
-            <Text fontSize={16} color={'muted.500'}>
-              {formatDate(review.updatedAt)}
-            </Text>
-          </VStack>
-        </HStack>
-        <Box>
-          <AirbnbRating
-            isDisabled={true}
-            selectedColor="#d6c103"
-            size={22}
-            defaultRating={review.stars}
-            showRating={false}
-          />
-        </Box>
-      </HStack>
-    </Box>
+      <Divider my={5} bg={'muted.400'} />
+      <AirbnbRating
+        isDisabled={true}
+        selectedColor="#d6c103"
+        size={22}
+        defaultRating={review.stars}
+        showRating={false}
+      />
+    </Flex>
   );
 };
