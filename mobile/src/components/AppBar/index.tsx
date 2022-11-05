9fa27d3900caad186fb89b246263c12ab5747120
João Bispo <joaobispo2077@gmail.com>
2022-11-05 13:23:02 -0300

    feat: show app bar infinity when dont have network connectivity

--- mobile/src/components/AppBar/index.tsx
+++ mobile/src/components/AppBar/index.tsx
@@ -1,50 +1,67 @@
 import { Feather } from '@expo/vector-icons';
 import { useNetInfo } from '@react-native-community/netinfo';
 import { Flex, Heading, Icon, StatusBar } from 'native-base';
+import { useEffect, useState } from 'react';
 
 import { ConditionallyRender } from '../ConditionallyRender';
-import { TimeoutRender } from '../TimeoutRender';
 
 export const AppBar = () => {
   const netInfo = useNetInfo();
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    setIsVisible(true);
+
+    const timeoutClear = setTimeout(() => {
+      setIsVisible(false);
+    }, 10000);
+
+    if (!netInfo.isConnected) {
+      clearTimeout(timeoutClear);
+    }
+
+    return () => {
+      if (timeoutClear && netInfo.isConnected) {
+        clearTimeout(timeoutClear);
+      }
+    };
+  }, [netInfo.isConnected]);
 
   return (
     <Flex>
       <StatusBar barStyle="light-content" backgroundColor="black" />
-      <TimeoutRender timeoutInSeconds={20} shoudlRenderInitially>
-        <ConditionallyRender
-          condition={Boolean(netInfo.isConnected)}
-          trueComponent={
-            <Flex
-              bg="green.500"
-              alignItems="center"
-              justifyContent="center"
-              direction="row"
-              h={25}
+      <ConditionallyRender
+        condition={isVisible}
+        trueComponent={
+          <Flex
+            bg={netInfo.isConnected ? 'green.500' : 'red.500'}
+            alignItems="center"
+            justifyContent="center"
+            direction="row"
+            h={25}
+          >
+            <Icon
+              as={Feather}
+              name={netInfo.isConnected ? 'wifi' : 'wifi-off'}
+              size="sm"
+              color="white"
+            />
+            <Heading
+              marginLeft={4}
+              color="white"
+              size="sm"
+              fontSize={14}
+              fontWeight="medium"
             >
-              <Icon as={Feather} name="wifi" size="sm" color="white" />
-              <Heading marginLeft={4} color="white" size="md">
-                Conectado: {netInfo.type}
-              </Heading>
-            </Flex>
-          }
-          falseComponent={
-            <Flex
-              bg="red.500"
-              alignItems="center"
-              justifyContent="center"
-              direction="row"
-              h={25}
-            >
-              <Icon as={Feather} name="wifi-off" size="sm" color="white" />
-              <Heading marginLeft={4} color="white" size="md">
-                Sem conexão
-              </Heading>
-            </Flex>
-          }
-        />
-      </TimeoutRender>
-      <Heading>diversaGente</Heading>
+              {netInfo.isConnected
+                ? `Conectado: ${netInfo.type.toString()}`
+                : 'Sem conexão com a internet'}
+            </Heading>
+          </Flex>
+        }
+        falseComponent={null}
+      />
+      {/* <Heading>diversaGente</Heading> */}
     </Flex>
   );
 };
